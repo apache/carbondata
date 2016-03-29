93283f6acd6fe7bb3cf33fea12b9a54aeacbd921
chenliang613 <chenliang613@huawei.com>
2016-03-29 12:05:59 +0530

    issue54:put examples to an individual directory

--- examples/src/main/scala/org/carbondata/examples/CarbonExample.scala
+++ examples/src/main/scala/org/carbondata/examples/CarbonExample.scala
@@ -1,4 +1,4 @@
-package com.huawei.carbondata.example
+package org.carbondata.CarbonExample
 
 import org.apache.spark.sql.CarbonContext
 import org.apache.spark.{SparkConf, SparkContext}
@@ -7,15 +7,20 @@ object CarbonExample {
 
   def main(args: Array[String]) {
 
+    //please replace with your local machine path
     val hdfsCarbonBasePath = "/home/root1/carbon/metadata"
     val sc = new SparkContext(new SparkConf()
       .setAppName("CarbonSpark")
       .setMaster("local[2]"))
 
     val oc = new CarbonContext(sc, hdfsCarbonBasePath)
+
+    //please replace with your local machine path
     oc.setConf("carbon.kettle.home", "/home/root1/carbon/carbondata/processing/carbonplugins/")
      oc.setConf("hive.metastore.warehouse.dir", "/home/root1/carbon/hivemetadata")
-//    oc.sql("drop cube alldatatypescube")
+
+    //When you excute the second time, need to enable it
+    //oc.sql("drop cube alldatatypescube")
 
     oc.sql("CREATE CUBE alldatatypescube DIMENSIONS (empno Integer, empname String, " +
       "designation String, doj Timestamp, workgroupcategory Integer, workgroupcategoryname String, " +
@@ -23,7 +28,8 @@ object CarbonExample {
       "projectenddate Timestamp) MEASURES (attendance Integer,utilization Integer,salary Integer) " +
       "OPTIONS (PARTITIONER [PARTITION_COUNT=1])")
 
-    oc.sql("LOAD DATA fact from '/home/root1/carbon/carbondata/integration/spark/src/test/resources/data.csv' INTO CUBE alldatatypescube PARTITIONDATA(DELIMITER ',', QUOTECHAR '\"')");
+    //please replace with your local machine path
+    oc.sql("LOAD DATA fact from '/home/root1/carbon/carbondata/examples/resources/data.csv' INTO CUBE alldatatypescube PARTITIONDATA(DELIMITER ',', QUOTECHAR '\"')");
 
     oc.sql("select empno,empname,utilization,count(salary),sum(empno) from alldatatypescube where empname in ('arvind','ayushi') group by empno,empname,utilization").show()
 
