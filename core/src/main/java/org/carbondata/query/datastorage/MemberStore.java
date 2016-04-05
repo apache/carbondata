258abcc71999ba4afb4dc5ebf8a7b196b617715e
mohammadshahidkhan <mohdshahidkhan1987@gmail.com>
2016-04-05 05:35:55 -0700

    Dictionary loopup for Dimension   Previously to get the surrogate/ id of the member a linear search was performed over the actual Member which was a costly operation.   The binary search can not be performed over the actual member since the members are unsorted. To address the same below code modification is done.     sortOrderIndex maintains the sorted surrogate based on the members actual value, so     the binary search is applied on sortOrderIndex to get the surrogate of the of requested member. + Implemented the binary search over 2 D array.   Added test case considering the scenario of incremental load for more than 10K data.

--- core/src/main/java/org/carbondata/query/datastorage/MemberStore.java
+++ core/src/main/java/org/carbondata/query/datastorage/MemberStore.java
@@ -58,6 +58,10 @@ public class MemberStore {
     private int[][] sortReverseOrderIndex;
 
     private int sortReverseOrderIndexSize;
+    /**
+     *  Total size of the sortOrderIndex[][]
+     */
+    private int sortOrderIndexSize;
 
     /**
      * Column name ffrom schema
@@ -165,6 +169,7 @@ public class MemberStore {
         globalCache = null;
         sortReverseOrderIndex = null;
         sortReverseOrderIndexSize = 0;
+        sortOrderIndexSize = 0;
     }
 
     /**
@@ -201,35 +206,37 @@ public class MemberStore {
             return 0;
         }
         byte[] nameChar = name.getBytes();
-        for (int index = 0; index < sortOrderIndex.length; index++) {
-            return getMemberId(sortOrderIndex[index], 0, sortOrderIndex[index].length, nameChar,
-                    cache[index]);
-        }
-        return 0;
+        return getMemberId(this.min, this.sortOrderIndexSize, nameChar);
     }
 
     /**
      * returns the surrogate of the requested member.
      */
-    private int getMemberId(int[] membersSortedByIndex, int fromIndex, int toIndex,
-            byte[] key, Member[] actualMembers) {
+    private int getMemberId(int fromIndex, int toIndex, byte[] key) {
         int low = fromIndex;
         int high = toIndex - 1;
 
         while (low <= high) {
             int mid = (low + high) >>> 1;
-            int surrogate = membersSortedByIndex[mid];
-            Member member = actualMembers[surrogate-min];
+            int surrogate = sortOrderIndex[mid / CarbonCommonConstants.LEVEL_ARRAY_SIZE][mid
+                    % CarbonCommonConstants.LEVEL_ARRAY_SIZE];
+            int dataIndex = surrogate - min;
+            int div = dataIndex / CarbonCommonConstants.LEVEL_ARRAY_SIZE;
+            int rem = dataIndex % CarbonCommonConstants.LEVEL_ARRAY_SIZE;
+
+            Member member = cache[div][rem];
             int cmp = ByteUtil.UnsafeComparer.INSTANCE.compareTo(member.getCharArray(), key);
-            if (cmp < 0)
+            if (cmp < 0) {
                 low = mid + 1;
-            else if (cmp > 0)
+            } else if (cmp > 0) {
                 high = mid - 1;
-            else
+            } else {
                 return surrogate; // key found
+            }
         }
         return 0;  // key not found.
     }
+
     /**
      * Get the sorted index of a surrogate
      *
@@ -379,6 +386,9 @@ public class MemberStore {
 
     public void setSortOrderIndex(int[][] sortOrderIndex) {
         this.sortOrderIndex = sortOrderIndex;
+        for (int i =0; i< this.sortOrderIndex.length; i++){
+            this.sortOrderIndexSize += this.sortOrderIndex[i].length;
+        }
     }
 
     public void setSortReverseOrderIndex(int[][] sortReverseOrderIndex) {
