1d8364d16336ddf64b4461e404e29bfecab1c45f
ashokblend <ashok.blend@gmail.com>
2016-04-30 11:13:33 +0530

    compilation fix (#282)
    moved java test cases to src/test/java folder

--- integration/spark/src/test/java/org/carbondata/integration/spark/testsuite/validation/FileFooterValidator.java
+++ integration/spark/src/test/java/org/carbondata/integration/spark/testsuite/validation/FileFooterValidator.java
@@ -1,12 +1,6 @@
 package org.carbondata.integration.spark.testsuite.validation;
 
-import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
-
-import org.apache.spark.sql.common.util.CarbonHiveContext;
 import org.carbondata.core.carbon.CarbonTableIdentifier;
-import org.carbondata.format.Encoding;
 import org.carbondata.core.carbon.path.CarbonStorePath;
 import org.carbondata.core.constants.CarbonCommonConstants;
 import org.carbondata.core.datastorage.store.FileHolder;
@@ -18,11 +12,13 @@ import org.carbondata.core.util.CarbonProperties;
 import org.carbondata.format.BlockletIndex;
 import org.carbondata.format.BlockletInfo;
 import org.carbondata.format.DataChunk;
+import org.carbondata.format.Encoding;
 import org.carbondata.format.FileFooter;
-import org.carbondata.query.util.DataFileFooterConverter;
+
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class FileFooterValidator {
 
@@ -30,27 +26,30 @@ public class FileFooterValidator {
 
   private static boolean setUpIsDone;
 
-  @Before
-  public void setUp() throws Exception {
+  @Before public void setUp() throws Exception {
 
     if (setUpIsDone) {
       return;
     }
-    CarbonHiveContext
-        .sql("CREATE CUBE validatefooter DIMENSIONS (empno Integer, empname String, designation String, doj Timestamp, workgroupcategory Integer, workgroupcategoryname String, deptno Integer, deptname String, projectcode Integer, projectjoindate Timestamp, projectenddate Timestamp) MEASURES (attendance Integer,utilization Integer,salary Integer) OPTIONS (PARTITIONER [PARTITION_COUNT=1])");
-    CarbonHiveContext
-        .sql("LOAD DATA fact from './src/test/resources/data.csv' INTO CUBE validatefooter PARTITIONDATA(DELIMITER ',', QUOTECHAR '\"')");
+    CarbonHiveContext.sql(
+            "CREATE CUBE validatefooter DIMENSIONS (empno Integer, empname String,"
+            + " designation String,"
+            + " doj Timestamp, workgroupcategory Integer, workgroupcategoryname String, "
+            + "deptno Integer, deptname String, projectcode Integer, projectjoindate Timestamp,"
+            + " projectenddate Timestamp) MEASURES (attendance Integer,utilization Integer,"
+            + "salary Integer) OPTIONS (PARTITIONER [PARTITION_COUNT=1])");
+    CarbonHiveContext.sql(
+            "LOAD DATA fact from './src/test/resources/data.csv' INTO CUBE validatefooter "
+                + "PARTITIONDATA(DELIMITER ',', QUOTECHAR '\"')");
     String storePath =
         CarbonProperties.getInstance().getProperty(CarbonCommonConstants.STORE_LOCATION);
     CarbonTableIdentifier tableIdentifier = new CarbonTableIdentifier("default", "validatefooter");
-    String segmentPath =
-        CarbonStorePath.getCarbonTablePath(storePath, tableIdentifier).getCarbonDataDirectoryPath(
-          "0", 0);
+    String segmentPath = CarbonStorePath.getCarbonTablePath(storePath, tableIdentifier)
+        .getCarbonDataDirectoryPath("0", 0);
     CarbonFile carbonFile =
         FileFactory.getCarbonFile(segmentPath, FileFactory.getFileType(segmentPath));
     CarbonFile[] list = carbonFile.listFiles(new CarbonFileFilter() {
-      @Override
-      public boolean accept(CarbonFile file) {
+      @Override public boolean accept(CarbonFile file) {
         if (file.getName().endsWith(CarbonCommonConstants.FACT_FILE_EXT)) {
           return true;
         }
@@ -67,50 +66,37 @@ public class FileFooterValidator {
       offset = fileHolder.readLong(fileLocation, offset);
       CarbonFooterReader metaDataReader = new CarbonFooterReader(fileLocation, offset);
       fileFooter = metaDataReader.readFooter();
-      testDataFileFooter(fileLocation, offset);
     }
     setUpIsDone = true;
   }
 
-  private static void testDataFileFooter(String fileLocation, long offset) throws IOException {
-    DataFileFooterConverter converter = new DataFileFooterConverter();
-    converter.readDataFileFooter(fileLocation, offset);
-  }
-
-  @AfterClass
-  public static void tearDownAfterClass() {
+  @AfterClass public static void tearDownAfterClass() {
     CarbonHiveContext.sql("drop CUBE validatefooter");
   }
 
-  @Test
-  public void testFileFooterExist() {
+  @Test public void testFileFooterExist() {
     assertTrue(fileFooter != null);
   }
 
-  @Test
-  public void testFileFooterVersion() {
+  @Test public void testFileFooterVersion() {
     assertTrue(fileFooter.getVersion() >= 0);
   }
 
-  @Test
-  public void testFileFooterNumRows() {
+  @Test public void testFileFooterNumRows() {
     assertTrue(fileFooter.getNum_rows() > 0);
   }
 
-  @Test
-  public void testFileFooterTableColumns() {
+  @Test public void testFileFooterTableColumns() {
     assertTrue(fileFooter.getTable_columns() != null && fileFooter.getTable_columns().size() > 0);
   }
 
-  @Test
-  public void testFileFooterSegmentInfo() {
-    assertTrue(fileFooter.getSegment_info() != null
-        && fileFooter.getSegment_info().getNum_cols() > 0
-        && fileFooter.getSegment_info().getColumn_cardinalities().size() > 0);
+  @Test public void testFileFooterSegmentInfo() {
+    assertTrue(
+        fileFooter.getSegment_info() != null && fileFooter.getSegment_info().getNum_cols() > 0
+            && fileFooter.getSegment_info().getColumn_cardinalities().size() > 0);
   }
 
-  @Test
-  public void testFileFooterBlockletIndex() {
+  @Test public void testFileFooterBlockletIndex() {
     assertTrue(fileFooter.getBlocklet_index_list() != null
         && fileFooter.getBlocklet_index_list().size() > 0);
     for (BlockletIndex blockletIndex : fileFooter.getBlocklet_index_list()) {
@@ -119,14 +105,13 @@ public class FileFooterValidator {
           && blockletIndex.getMin_max_index().getMax_values() != null
           && blockletIndex.getMin_max_index().getMax_values().size() > 0
           && blockletIndex.getMin_max_index().getMin_values().size() == blockletIndex
-              .getMin_max_index().getMax_values().size());
+          .getMin_max_index().getMax_values().size());
       assertTrue(blockletIndex.getB_tree_index().getStart_key() != null
           && blockletIndex.getB_tree_index().getEnd_key() != null);
     }
   }
 
-  @Test
-  public void testFileFooterBlockletInfo() {
+  @Test public void testFileFooterBlockletInfo() {
     assertTrue(fileFooter.getBlocklet_info_list() != null
         && fileFooter.getBlocklet_info_list().size() > 0);
     for (BlockletInfo blockletInfo : fileFooter.getBlocklet_info_list()) {
@@ -143,7 +128,8 @@ public class FileFooterValidator {
         && columnDatachunk.getChunk_meta().getCompression_codec() != null);
     // For Measure
     if (columnDatachunk.getEncoders().contains(Encoding.DELTA)) {
-      assertTrue(columnDatachunk.getPresence() != null && columnDatachunk.getEncoder_meta() != null);
+      assertTrue(
+          columnDatachunk.getPresence() != null && columnDatachunk.getEncoder_meta() != null);
     } else {
       assertTrue(columnDatachunk.getSort_state() != null);
     }
