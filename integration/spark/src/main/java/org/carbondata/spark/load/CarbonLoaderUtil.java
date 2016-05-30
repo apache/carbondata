d3db227e76d0824d7fa620b4f5d7223542792d03
Mohammad Shahid Khan <mohdshahidkhan1987@gmail.com>
2016-05-30 18:19:20 +0530

    [Issue - 531] The error of '/ by zero' will random occurs when query from the table (#532)

--- integration/spark/src/main/java/org/carbondata/spark/load/CarbonLoaderUtil.java
+++ integration/spark/src/main/java/org/carbondata/spark/load/CarbonLoaderUtil.java
@@ -18,25 +18,9 @@
  */
 package org.carbondata.spark.load;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import org.carbondata.common.logging.LogService;
 import org.carbondata.common.logging.LogServiceFactory;
@@ -959,7 +943,9 @@ public final class CarbonLoaderUtil {
 
     Map<String, List<TableBlockInfo>> mapOfNodes =
         CarbonLoaderUtil.nodeBlockMapping(blockInfos, noOfNodesInput);
-    int noOfTasksPerNode = parallelism / mapOfNodes.size();
+    int taskPerNode = parallelism / mapOfNodes.size();
+    //assigning non zero value to noOfTasksPerNode
+    int noOfTasksPerNode = taskPerNode == 0 ? 1 : taskPerNode;
     // divide the blocks of a node among the tasks of the node.
     return assignBlocksToTasksPerNode(mapOfNodes, noOfTasksPerNode);
   }
