cdfd08c81a7df47bd99f145e1119fcdb21a8df6c
linyixin <linyixin@huawei.com>
2016-06-28 01:04:55 +0800

    [Bug]fix bug in handle multiple quotes (#761)
    When handle data with quotes,we need to follow the rules:
    1)the cell should be enclosed in quote character
    2)inside the data eache quote is escaped using escape character.
    
    for example:
    """abc""" -> "abc" (escape char and quote is ")
    ”\"i\"" ->"i" (escape char is\ ,quote is ")

--- processing/src/main/java/org/carbondata/processing/csvreaderstep/CsvInputData.java
+++ processing/src/main/java/org/carbondata/processing/csvreaderstep/CsvInputData.java
@@ -45,6 +45,7 @@ public class CsvInputData extends BaseStepData implements StepDataInterface {
   public EncodingType encodingType;
   public PatternMatcherInterface delimiterMatcher;
   public PatternMatcherInterface enclosureMatcher;
+  public PatternMatcherInterface escapeCharMatcher;
   public CrLfMatcherInterface crLfMatcher;
 
   public CsvInputData() {
