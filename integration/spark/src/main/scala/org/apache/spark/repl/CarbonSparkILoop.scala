1730af3567865fe4d6c33adde4a4f7868d4006c4
ravipesala <ravi.pesala@gmail.com>
2016-07-11 15:56:20 +0530

    Added Carbon spark shell

--- integration/spark/src/main/scala/org/apache/spark/repl/CarbonSparkILoop.scala
+++ integration/spark/src/main/scala/org/apache/spark/repl/CarbonSparkILoop.scala
@@ -1,8 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.repl
 
 class CarbonSparkILoop extends SparkILoop {
-
-
 
   override def initializeSpark() {
     intp.beQuietDuring {
@@ -24,11 +39,19 @@ class CarbonSparkILoop extends SparkILoop {
       command("import org.apache.spark.sql.CarbonContext")
       command("""
          @transient val cc = {
-           val _cc = new CarbonContext(sc)
+           val _cc = {
+             import java.io.File
+             val store = new File("../carbonshellstore")
+             store.mkdirs()
+             val storePath = sc.getConf.getOption("spark.carbon.storepath")
+                  .getOrElse(store.getCanonicalPath)
+             new CarbonContext(sc, storePath, store.getCanonicalPath)
+           }
            println("Carbon context available as cc.")
            _cc
          }
               """)
+      command("""cc.setConf("carbon.kettle.home", "../processing/carbonplugins")""")
       command("import cc.implicits._")
       command("import cc.sql")
       command("import org.apache.spark.sql.functions._")
