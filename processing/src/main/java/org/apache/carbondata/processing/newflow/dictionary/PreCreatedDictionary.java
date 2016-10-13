e44f7a9cd0441e46437418b71d83fbdb1ebda7cf
jackylk <jacky.likun@huawei.com>
2016-10-14 01:38:43 +0800

    add dictionary interface
    remove size()
    
    add PreCreatedDictionary.java
    
    add size()
    
    fix comment

---
+++ processing/src/main/java/org/apache/carbondata/processing/newflow/dictionary/PreCreatedDictionary.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.carbondata.processing.newflow.dictionary;
+
+import org.apache.carbondata.core.cache.dictionary.Dictionary;
+import org.apache.carbondata.core.devapi.BiDictionary;
+
+public class PreCreatedDictionary implements BiDictionary<Integer, String> {
+
+  private Dictionary dictionary;
+
+  public PreCreatedDictionary(Dictionary dictionary) {
+    this.dictionary = dictionary;
+  }
+
+  @Override
+  public Integer getOrGenerateKey(String value) throws Exception {
+    Integer key = getKey(value);
+    if (key == null) {
+      throw new UnsupportedOperationException("trying to add new entry in PreCreatedDictionary");
+    }
+    return key;
+  }
+
+  @Override
+  public Integer getKey(String value) {
+    return dictionary.getSurrogateKey(value);
+  }
+
+  @Override
+  public String getValue(Integer key) {
+    return dictionary.getDictionaryValueForKey(key);
+  }
+
+  @Override
+  public int size() {
+    return dictionary.getDictionaryChunks().getSize();
+  }
+}
