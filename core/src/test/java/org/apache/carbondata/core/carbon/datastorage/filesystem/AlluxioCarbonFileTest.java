0a7868ba4e297374ec1fa5eb9fa9c64d60c6ebfa
anubhav100 <anubhav.tarar@knoldus.in>
2016-11-15 14:40:34 +0530

    added apache documentation in test files

--- core/src/test/java/org/apache/carbondata/core/carbon/datastorage/filesystem/AlluxioCarbonFileTest.java
+++ core/src/test/java/org/apache/carbondata/core/carbon/datastorage/filesystem/AlluxioCarbonFileTest.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.carbondata.core.carbon.datastorage.filesystem;
 
 import mockit.Mock;
@@ -228,7 +247,7 @@ public class AlluxioCarbonFileTest {
         };
         new MockUp<DistributedFileSystem>() {
             @Mock
-            public FileStatus getFileStatus(Path f) throws IOException {
+            public FileStatus getFileStatus(Path path) throws IOException {
 
                 return new FileStatus(12L, true, 60, 120l, 180L, new Path(file.getAbsolutePath()));
             }
