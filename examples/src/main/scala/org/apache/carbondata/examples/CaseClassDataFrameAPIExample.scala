320a384bb1df904bfc1db67c0ad554bea38657d7
SangeetaGulia <sangeeta.gulia@knoldus.in>
2016-11-18 17:49:32 +0530

    Changed import order in CaseClassDataFrameAPIExample

--- examples/src/main/scala/org/apache/carbondata/examples/CaseClassDataFrameAPIExample.scala
+++ examples/src/main/scala/org/apache/carbondata/examples/CaseClassDataFrameAPIExample.scala
@@ -17,9 +17,11 @@
 
 package org.apache.carbondata.examples
 
-import org.apache.carbondata.examples.util.ExampleUtils
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SaveMode}
+
+import org.apache.spark.rdd.RDD
+
+import org.apache.carbondata.examples.util.ExampleUtils
 
 case class People(name: String, occupation: String, id: Int)
 
