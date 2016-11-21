320f2b4a4a466838819b5673083664afd8cc2170
abhishek <abhishek@knoldus.com>
2016-11-21 12:18:13 +0530

    class formatted and CarbonmetaDataUtil test added

--- core/src/test/java/org/apache/carbondata/core/util/CarbonMergerUtilTest.java
+++ core/src/test/java/org/apache/carbondata/core/util/CarbonMergerUtilTest.java
@@ -20,28 +20,24 @@
 package org.apache.carbondata.core.util;
 
 import static junit.framework.TestCase.*;
+
 import mockit.Mock;
 import mockit.MockUp;
 import org.junit.Test;
 
 import static org.apache.carbondata.core.util.CarbonMergerUtil.getCardinalityFromLevelMetadata;
 
-
 public class CarbonMergerUtilTest {
 
-
-
-    @Test
-    public void testGetCardinalityFromLevelMetadata() throws Exception{
-        final int[] localCardinality = {1,2,3,4,5,6};
-        new MockUp<CarbonUtil>() {
-            @SuppressWarnings("unused")
-            @Mock
-            public int[] getCardinalityFromLevelMetadataFile(String levelPath){
-                return  localCardinality;
-            }
-        };
-        int[] result= getCardinalityFromLevelMetadata("STORE_PATH","table1");
-        assertEquals(result,localCardinality);
-    }
+  @Test public void testGetCardinalityFromLevelMetadata() throws Exception {
+    final int[] localCardinality = { 1, 2, 3, 4, 5, 6 };
+    new MockUp<CarbonUtil>() {
+      @SuppressWarnings("unused") @Mock
+      public int[] getCardinalityFromLevelMetadataFile(String levelPath) {
+        return localCardinality;
+      }
+    };
+    int[] result = getCardinalityFromLevelMetadata("STORE_PATH", "table1");
+    assertEquals(result, localCardinality);
+  }
 }
