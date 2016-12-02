cee7df45ed06e020499bea9f0667b6470d01bb78
Geetika gupta <geetika.gupta@knoldus.in>
2016-12-02 15:53:01 +0530

    Closed CarbonData-483 Added testcases for core.carbon.metadata

--- core/src/test/java/org/apache/carbondata/core/carbon/metadata/CarbonMetadataTest.java
+++ core/src/test/java/org/apache/carbondata/core/carbon/metadata/CarbonMetadataTest.java
@@ -24,29 +24,37 @@ import java.util.UUID;
 
 import org.apache.carbondata.core.carbon.metadata.datatype.DataType;
 import org.apache.carbondata.core.carbon.metadata.encoder.Encoding;
+import org.apache.carbondata.core.carbon.metadata.schema.table.CarbonTable;
 import org.apache.carbondata.core.carbon.metadata.schema.table.TableInfo;
 import org.apache.carbondata.core.carbon.metadata.schema.table.TableSchema;
+import org.apache.carbondata.core.carbon.metadata.schema.table.column.CarbonDimension;
 import org.apache.carbondata.core.carbon.metadata.schema.table.column.ColumnSchema;
 import org.apache.carbondata.core.constants.CarbonCommonConstants;
 
 import junit.framework.TestCase;
+import mockit.Mock;
+import mockit.MockUp;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class CarbonMetadataTest extends TestCase {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-  private CarbonMetadata carbonMetadata;
+public class CarbonMetadataTest {
 
-  private String tableUniqueName;
+  private static CarbonMetadata carbonMetadata;
 
-  @BeforeClass public void setUp() {
+  private static String tableUniqueName;
+
+  @BeforeClass public static void setUp() {
     carbonMetadata = CarbonMetadata.getInstance();
     carbonMetadata.loadTableMetadata(getTableInfo(10000));
     tableUniqueName = "carbonTestDatabase_carbonTestTable";
   }
 
-  @AfterClass public void tearDown() {
+  @AfterClass public static void tearDown() {
     carbonMetadata.removeTable(tableUniqueName);
     carbonMetadata = null;
   }
@@ -101,7 +109,7 @@ public class CarbonMetadataTest extends TestCase {
     assertEquals(1, carbonMetadata.getNumberOfTables());
   }
 
-  private ColumnSchema getColumnarDimensionColumn() {
+  private static ColumnSchema getColumnarDimensionColumn() {
     ColumnSchema dimColumn = new ColumnSchema();
     dimColumn.setColumnar(true);
     dimColumn.setColumnName("IMEI");
@@ -116,7 +124,7 @@ public class CarbonMetadataTest extends TestCase {
     return dimColumn;
   }
 
-  private ColumnSchema getColumnarMeasureColumn() {
+  private static ColumnSchema getColumnarMeasureColumn() {
     ColumnSchema dimColumn = new ColumnSchema();
     dimColumn.setAggregateFunction("SUM");
     dimColumn.setColumnName("IMEI_COUNT");
@@ -125,7 +133,7 @@ public class CarbonMetadataTest extends TestCase {
     return dimColumn;
   }
 
-  private TableSchema getTableSchema() {
+  private static TableSchema getTableSchema() {
     TableSchema tableSchema = new TableSchema();
     List<ColumnSchema> columnSchemaList = new ArrayList<ColumnSchema>();
     columnSchemaList.add(getColumnarDimensionColumn());
@@ -136,7 +144,7 @@ public class CarbonMetadataTest extends TestCase {
     return tableSchema;
   }
 
-  private TableInfo getTableInfo(long timeStamp) {
+  private static TableInfo getTableInfo(long timeStamp) {
     TableInfo info = new TableInfo();
     info.setDatabaseName("carbonTestDatabase");
     info.setLastUpdatedTime(timeStamp);
@@ -145,4 +153,107 @@ public class CarbonMetadataTest extends TestCase {
     info.setStorePath("/test/store");
     return info;
   }
+
+  @Test public void testGetCarbonDimensionBasedOnColIdentifier() {
+    CarbonTable carbonTable = new CarbonTable();
+    String columnIdentifier = "1";
+    final List<CarbonDimension> carbonDimensions = new ArrayList();
+    ColumnSchema colSchema1 = new ColumnSchema();
+    ColumnSchema colSchema2 = new ColumnSchema();
+    colSchema1.setColumnUniqueId("1");
+    colSchema2.setColumnUniqueId("2");
+    carbonDimensions.add(new CarbonDimension(colSchema1, 1, 1, 2, 1));
+    carbonDimensions.add(new CarbonDimension(colSchema2, 2, 2, 2, 2));
+    new MockUp<CarbonTable>() {
+      @Mock public String getFactTableName() {
+        return "carbonTestTable";
+      }
+
+      @Mock public List<CarbonDimension> getDimensionByTableName(String tableName) {
+        return carbonDimensions;
+      }
+    };
+    CarbonDimension expectedResult = carbonDimensions.get(0);
+    CarbonDimension actualResult =
+        carbonMetadata.getCarbonDimensionBasedOnColIdentifier(carbonTable, columnIdentifier);
+    assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  public void testGetCarbonDimensionBasedOnColIdentifierWhenChildDimensionColumnEqualsColumnIdentifier() {
+    CarbonTable carbonTable = new CarbonTable();
+    String columnIdentifier = "9";
+    final List<CarbonDimension> carbonDimensions = new ArrayList();
+    ColumnSchema colSchema1 = new ColumnSchema();
+    ColumnSchema colSchema2 = new ColumnSchema();
+    colSchema1.setColumnUniqueId("1");
+    carbonDimensions.add(new CarbonDimension(colSchema1, 1, 1, 2, 1));
+    final List<CarbonDimension> carbonChildDimensions = new ArrayList();
+    ColumnSchema colSchema3 = new ColumnSchema();
+    colSchema3.setColumnUniqueId("9");
+    colSchema2.setColumnUniqueId("2");
+    carbonChildDimensions.add(new CarbonDimension(colSchema3, 1, 1, 2, 1));
+    new MockUp<CarbonTable>() {
+      @Mock public String getFactTableName() {
+        return "carbonTestTable";
+      }
+
+      @Mock public List<CarbonDimension> getDimensionByTableName(String tableName) {
+        return carbonDimensions;
+      }
+    };
+
+    new MockUp<CarbonDimension>() {
+      @Mock public int numberOfChild() {
+        return 1;
+      }
+
+      @Mock public List<CarbonDimension> getListOfChildDimensions() {
+        return carbonChildDimensions;
+      }
+
+    };
+
+    CarbonDimension expectedResult = carbonChildDimensions.get(0);
+    CarbonDimension actualResult =
+        carbonMetadata.getCarbonDimensionBasedOnColIdentifier(carbonTable, columnIdentifier);
+    assertEquals(expectedResult, actualResult);
+  }
+
+  @Test public void testGetCarbonDimensionBasedOnColIdentifierNullCase() {
+    CarbonTable carbonTable = new CarbonTable();
+    String columnIdentifier = "3";
+    final List<CarbonDimension> carbonDimensions = new ArrayList();
+    ColumnSchema colSchema1 = new ColumnSchema();
+    colSchema1.setColumnUniqueId("1");
+    colSchema1.setNumberOfChild(1);
+    CarbonDimension carbonDimension = new CarbonDimension(colSchema1, 1, 1, 2, 1);
+    carbonDimensions.add(carbonDimension);
+    final List<CarbonDimension> carbonChildDimensions = new ArrayList();
+    ColumnSchema colSchema2 = new ColumnSchema();
+    colSchema2.setColumnUniqueId("9");
+    colSchema2.setNumberOfChild(0);
+    carbonChildDimensions.add(new CarbonDimension(colSchema2, 1, 1, 2, 1));
+
+    new MockUp<CarbonTable>() {
+      @Mock public String getFactTableName() {
+        return "carbonTestTable";
+      }
+
+      @Mock public List<CarbonDimension> getDimensionByTableName(String tableName) {
+        return carbonDimensions;
+      }
+    };
+
+    new MockUp<CarbonDimension>() {
+      @Mock public List<CarbonDimension> getListOfChildDimensions() {
+        return carbonChildDimensions;
+      }
+    };
+
+    CarbonDimension result =
+        carbonMetadata.getCarbonDimensionBasedOnColIdentifier(carbonTable, columnIdentifier);
+    assertNull(result);
+  }
+
 }
