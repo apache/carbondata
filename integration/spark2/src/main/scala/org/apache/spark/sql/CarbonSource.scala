dbd4b1c538153f32098bfd7d4843679e517e2d88
ravipesala <ravi.pesala@gmail.com>
2016-12-02 23:02:18 +0530

    rebased

--- integration/spark2/src/main/scala/org/apache/spark/sql/CarbonSource.scala
+++ integration/spark2/src/main/scala/org/apache/spark/sql/CarbonSource.scala
@@ -132,7 +132,8 @@ class CarbonSource extends CreatableRelationProvider
         }
         val map = scala.collection.mutable.Map[String, String]()
         parameters.foreach { x => map.put(x._1, x._2) }
-        val cm = TableCreator.prepareTableModel(false, Option(dbName), tableName, fields, Nil, map)
+        val cm = TableCreator.prepareTableModel(false, Option(dbName),
+          tableName, fields, Nil, None, map)
         CreateTable(cm, false).run(sparkSession)
         CarbonEnv.get.carbonMetastore.storePath + s"/$dbName/$tableName"
       case ex: Exception =>
