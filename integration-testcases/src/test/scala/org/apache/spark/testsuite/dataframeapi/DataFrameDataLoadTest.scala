7bacc127a318c944cb158afa583e922e790e55b1
Harsh Sharma <harsh@knoldus.com>
2016-12-19 16:26:04 +0530

    created the suite for DataFrame API tests

---
+++ integration-testcases/src/test/scala/org/apache/spark/testsuite/dataframeapi/DataFrameDataLoadTest.scala
@@ -0,0 +1,11 @@
+package org.apache.spark.testsuite.dataframeapi
+
+import org.scalatest.FlatSpec
+
+class DataFrameDataLoadTest extends FlatSpec {
+
+  "CarbonData" should "be able to load data from a table and execute queries on it" in {
+    assert(true)
+  }
+
+}
