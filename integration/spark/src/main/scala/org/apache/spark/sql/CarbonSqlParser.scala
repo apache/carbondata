249d573619950075de99232e36ef9c7a9352229c
anubhav100 <anubhav.tarar@knoldus.in>
2017-01-13 12:29:03 +0530

    resolve java style error

--- integration/spark/src/main/scala/org/apache/spark/sql/CarbonSqlParser.scala
+++ integration/spark/src/main/scala/org/apache/spark/sql/CarbonSqlParser.scala
@@ -257,7 +257,8 @@ class CarbonSqlParser() extends CarbonDDLSqlParser {
             Token("TOK_TABCOLNAME", list)::numberOfBuckets) =>
               val cols = list.map(_.getText)
               if (cols != null) {
-                throw new MalformedCarbonCommandException("Bucketing Is not Supported in spark 1.6 ")
+                throw new MalformedCarbonCommandException (
+                  "Bucketing Is not Supported in spark 1.6 ")
                 bucketFields = Some(BucketFields(cols,
                   numberOfBuckets.head.getText.toInt))
               }
