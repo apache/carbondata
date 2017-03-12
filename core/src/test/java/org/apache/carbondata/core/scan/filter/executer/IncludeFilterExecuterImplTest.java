c74fbfae4e399c271e7cb59ccf14e50b55755e1e
mayun <simafengyun1984@163.com>
2017-03-12 17:43:14 +0800

    add comments for test case make it is easy to read

--- core/src/test/java/org/apache/carbondata/core/scan/filter/executer/IncludeFilterExecuterImplTest.java
+++ core/src/test/java/org/apache/carbondata/core/scan/filter/executer/IncludeFilterExecuterImplTest.java
@@ -77,31 +77,42 @@ public class IncludeFilterExecuterImplTest extends TestCase {
   }
 
   /**
-   * short int to byte
+   * change int to byte[]
    * 
-   * @param s
-   *          short int
+   * @param value
+   * @param size
    * @return byte[]
    */
-  private byte[] unsignedShortToByte2(int s) {
-    byte[] targets = new byte[2];
-    targets[0] = (byte) (s >> 8 & 0xFF);
-    targets[1] = (byte) (s & 0xFF);
+  private byte[] transferIntToByteArr(int value, int size) {
+    byte[] targets = new byte[size];
+    for (int i = 0; i < size; i++) {
+      int data = value;
+      for (int j = i; j < size - 1; j++) {
+        data = data >> 8;
+      }
+      data = data & 0xFF;
+      targets[i] = (byte) (data & 0xFF);
+    }
     return targets;
   }
 
   @Test
   public void testPerformance() {
 
-    int dataCnt = 120000;
-    int queryCnt = 5;
-    int repeatCnt = 200;
-    
+    // dimension's data number in a blocklet, usually default is 120000
+    int dataChunkSize = 120000; 
+    //  repeat query times in the test
+    int queryTimes = 5;    
+    // repeated times for a dictionary value
+    int repeatTimes = 200;
+    //filtered value count in a blocklet
     int filteredValueCnt = 800;
-    comparePerformance(dataCnt, filteredValueCnt, queryCnt, repeatCnt);
+    comparePerformance(dataChunkSize, filteredValueCnt, queryTimes, repeatTimes);
     
     filteredValueCnt = 100;
-    comparePerformance(dataCnt, filteredValueCnt, queryCnt, repeatCnt);
+    // set big repeat value for test dimension dictionary size is 1
+    repeatTimes = 2000;
+    comparePerformance(dataChunkSize, filteredValueCnt, queryTimes, repeatTimes);
 
   }
   
@@ -113,16 +124,16 @@ public class IncludeFilterExecuterImplTest extends TestCase {
 
 	// dimension's data number in a blocklet, usually default is 120000
     int dataChunkSize = 120000; 
-    //  repeat query count in the test
-    int queryCnt = 5;    
-    // use to generate repeated dictionary value
-    int repeatCnt = 20;
+    //  repeat query times in the test
+    int queryTimes = 5;    
+    // repeated times for a dictionary value
+    int repeatTimes = 20;
     //filtered value count in a blocklet
     int filteredValueCnt = 1;
-    comparePerformance(dataChunkSize, filteredValueCnt, queryCnt, repeatCnt);
+    comparePerformance(dataChunkSize, filteredValueCnt, queryTimes, repeatTimes);
     
     filteredValueCnt = 0;
-    comparePerformance(dataChunkSize, filteredValueCnt, queryCnt, repeatCnt);
+    comparePerformance(dataChunkSize, filteredValueCnt, queryTimes, repeatTimes);
 
   }
 
@@ -131,37 +142,40 @@ public class IncludeFilterExecuterImplTest extends TestCase {
    * 
    * @param dataChunkSize dataChunk's stored data size
    * @param filteredValueCnt filtered dictionary value count
-   * @param queryCnt repeat query count in the test
-   * @param repeatCnt use to generate repeated test dictionary value
+   * @param queryTimes repeat query times in the test
+   * @param repeatTimes repeated times for a dictionary value
    * @return 
    */
   private void comparePerformance(int dataChunkSize, int filteredValueCnt,
-      int queryCnt, int repeatCnt) {
+      int queryTimes, int repeatTimes) {
     long start;
     long oldTime = 0;
     long newTime = 0;
-    
-    // column size
+    int baseFilteredValue = 100;
+    // column dictionary size
     int dimColumnSize = 2;
+    if ((dataChunkSize / repeatTimes) <= 255 && (baseFilteredValue+filteredValueCnt) <= 255) {
+      dimColumnSize = 1;
+    }
+    System.out.println("dimColumnSize: " + dimColumnSize);
+    
     FixedLengthDimensionDataChunk dimensionColumnDataChunk;
     DimColumnExecuterFilterInfo dim = new DimColumnExecuterFilterInfo();
 
     byte[] dataChunk = new byte[dataChunkSize * dimColumnSize];
     for (int i = 0; i < dataChunkSize; i++) {
-
-      if (i % repeatCnt == 0) {
-        repeatCnt++;
+      if (i % repeatTimes == 0) {
+        repeatTimes++;
       }
-
-      byte[] data = unsignedShortToByte2(repeatCnt);
-      dataChunk[2 * i] = data[0];
-      dataChunk[2 * i + 1] = data[1];
-
+      byte[] data = transferIntToByteArr(repeatTimes, dimColumnSize);      
+      for(int j =0 ; j< dimColumnSize;j++){      
+        dataChunk[dimColumnSize * i + j] = data[j];
+      }
     }
 
     byte[][] filterKeys = new byte[filteredValueCnt][2];
-    for (int ii = 0; ii < filteredValueCnt; ii++) {
-      filterKeys[ii] = unsignedShortToByte2(100 + ii);
+    for (int k = 0; k < filteredValueCnt; k++) {
+      filterKeys[k] = transferIntToByteArr(baseFilteredValue+k, dimColumnSize);
     }
     dim.setFilterKeys(filterKeys);
 
@@ -169,7 +183,7 @@ public class IncludeFilterExecuterImplTest extends TestCase {
         dataChunkSize, dimColumnSize);
 
     // repeat query and compare 2 result between old code and new optimized code
-    for (int j = 0; j < queryCnt; j++) {
+    for (int j = 0; j < queryTimes; j++) {
 
       start = System.currentTimeMillis();
       BitSet bitOld = this.setFilterdIndexToBitSet(dimensionColumnDataChunk, dataChunkSize, filterKeys);
@@ -181,7 +195,6 @@ public class IncludeFilterExecuterImplTest extends TestCase {
       newTime = newTime + System.currentTimeMillis() - start;
 
       assertTrue(bitOld.equals(bitNew));
-
     }
 
     if (filteredValueCnt >= 100) {
@@ -252,50 +265,56 @@ public class IncludeFilterExecuterImplTest extends TestCase {
     long newTime = 0;
     long start;
     long end;
-    int dataCnt = 120000;
-    int filterCnt = 800;
-    int queryCnt = 10000;
-    int repeatCnt = 200;
-    byte[] keyWord = new byte[2];
+    
+    // dimension's data number in a blocklet, usually default is 120000
+    int dataChunkSize = 120000; 
+    //  repeat query times in the test
+    int queryTimes = 10000;    
+    // repeated times for a dictionary value
+    int repeatTimes = 200;
+    //filtered value count in a blocklet
+    int filteredValueCnt = 800;
+    // column dictionary size
+    int dimColumnSize = 2;
     FixedLengthDimensionDataChunk dimensionColumnDataChunk;
     DimColumnExecuterFilterInfo dim = new DimColumnExecuterFilterInfo();
 
-    byte[] dataChunk = new byte[dataCnt * keyWord.length];
-    for (int i = 0; i < dataCnt; i++) {
+    byte[] dataChunk = new byte[dataChunkSize * dimColumnSize];
+    for (int i = 0; i < dataChunkSize; i++) {
 
-      if (i % repeatCnt == 0) {
-        repeatCnt++;
+      if (i % repeatTimes == 0) {
+        repeatTimes++;
       }
 
-      byte[] data = unsignedShortToByte2(repeatCnt);
+      byte[] data = transferIntToByteArr(repeatTimes, dimColumnSize);
       dataChunk[2 * i] = data[0];
       dataChunk[2 * i + 1] = data[1];
 
     }
 
-    byte[][] filterKeys = new byte[filterCnt][2];
-    for (int ii = 0; ii < filterCnt; ii++) {
-      filterKeys[ii] = unsignedShortToByte2(100 + ii);
+    byte[][] filterKeys = new byte[filteredValueCnt][2];
+    for (int k = 0; k < filteredValueCnt; k++) {
+      filterKeys[k] = transferIntToByteArr(100 + k, dimColumnSize);
     }
     dim.setFilterKeys(filterKeys);
 
     dimensionColumnDataChunk = new FixedLengthDimensionDataChunk(dataChunk, null, null,
-        dataChunk.length / keyWord.length, keyWord.length);
+        dataChunk.length / dimColumnSize, dimColumnSize);
 
     // initial to run
-    BitSet bitOld = this.setFilterdIndexToBitSetWithColumnIndexOld(dimensionColumnDataChunk, dataCnt, filterKeys);
-    BitSet bitNew = this.setFilterdIndexToBitSetWithColumnIndexNew(dimensionColumnDataChunk, dataCnt, filterKeys);
+    BitSet bitOld = this.setFilterdIndexToBitSetWithColumnIndexOld(dimensionColumnDataChunk, dataChunkSize, filterKeys);
+    BitSet bitNew = this.setFilterdIndexToBitSetWithColumnIndexNew(dimensionColumnDataChunk, dataChunkSize, filterKeys);
 
     // performance run
-    for (int j = 0; j < queryCnt; j++) {
+    for (int j = 0; j < queryTimes; j++) {
 
       start = System.currentTimeMillis();
-      bitOld = this.setFilterdIndexToBitSetWithColumnIndexOld(dimensionColumnDataChunk, dataCnt, filterKeys);
+      bitOld = this.setFilterdIndexToBitSetWithColumnIndexOld(dimensionColumnDataChunk, dataChunkSize, filterKeys);
       end = System.currentTimeMillis();
       oldTime = oldTime + end - start;
 
       start = System.currentTimeMillis();
-      bitNew = this.setFilterdIndexToBitSetWithColumnIndexNew(dimensionColumnDataChunk, dataCnt, filterKeys);
+      bitNew = this.setFilterdIndexToBitSetWithColumnIndexNew(dimensionColumnDataChunk, dataChunkSize, filterKeys);
       end = System.currentTimeMillis();
       newTime = newTime + end - start;
 
