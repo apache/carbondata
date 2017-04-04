98b5b3b31a20cff85306a43a6925ab89ea9554a9
chenliang613 <chenliang613@huawei.com>
2017-04-04 20:42:44 +0530

    makes trivial change for sync up apache git to github

--- examples/spark/src/main/scala/org/apache/carbondata/examples/AllDictionaryExample.scala
+++ examples/spark/src/main/scala/org/apache/carbondata/examples/AllDictionaryExample.scala
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.carbondata.examples
 
 import org.apache.carbondata.core.constants.CarbonCommonConstants
