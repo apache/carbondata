9dfae66b78f6134f179f3ef2dd8e36d135c28aa2
anubhav100 <anubhav.tarar@knoldus.in>
2017-05-05 17:31:43 +0530

    resolved bug for able to load data with byte type from dataframe in carbon table

--- integration/spark/src/main/scala/org/apache/carbondata/spark/CarbonDataFrameWriter.scala
+++ integration/spark/src/main/scala/org/apache/carbondata/spark/CarbonDataFrameWriter.scala
@@ -161,7 +161,6 @@ class CarbonDataFrameWriter(val dataFrame: DataFrame) {
     sparkType match {
       case StringType => CarbonType.STRING.getName
       case IntegerType => CarbonType.INT.getName
-      case ByteType => CarbonType.INT.getName
       case ShortType => CarbonType.SHORT.getName
       case LongType => CarbonType.LONG.getName
       case FloatType => CarbonType.DOUBLE.getName
