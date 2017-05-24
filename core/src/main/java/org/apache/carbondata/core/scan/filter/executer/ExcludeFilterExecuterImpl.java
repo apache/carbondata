d050de9f9acfdaebf3353cfc3a94e2f8b3381c04
mayun <simafengyun1984@163.com>
2017-05-24 14:04:43 +0800

    use binarySearch to replace for clause to improve performance

--- core/src/main/java/org/apache/carbondata/core/scan/filter/executer/ExcludeFilterExecuterImpl.java
+++ core/src/main/java/org/apache/carbondata/core/scan/filter/executer/ExcludeFilterExecuterImpl.java
@@ -146,9 +146,17 @@ public class ExcludeFilterExecuterImpl implements FilterExecuter {
     BitSet bitSet = new BitSet(numerOfRows);
     bitSet.flip(0, numerOfRows);
     byte[][] filterValues = dimColumnExecuterInfo.getFilterKeys();
-    for (int k = 0; k < filterValues.length; k++) {
+    if (filterValues.length > 1) {
       for (int j = 0; j < numerOfRows; j++) {
-        if (dimColumnDataChunk.compareTo(j, filterValues[k]) == 0) {
+        int index = CarbonUtil.binarySearch(filterValues, 0, filterValues.length - 1,
+            dimColumnDataChunk.getChunkData(j));
+        if (index >= 0) {
+          bitSet.flip(j);
+        }
+      }
+    } else if (filterValues.length == 1) {
+      for (int j = 0; j < numerOfRows; j++) {
+        if (dimColumnDataChunk.compareTo(j, filterValues[0]) == 0) {
           bitSet.flip(j);
         }
       }
