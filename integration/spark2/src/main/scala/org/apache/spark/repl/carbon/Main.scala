be00058eb1abecd9e87afc530176c781df480513
xuchuanyin <xuchuanyin@126.com>
2017-05-27 17:18:08 +0800

    reformat code

--- integration/spark2/src/main/scala/org/apache/spark/repl/carbon/Main.scala
+++ integration/spark2/src/main/scala/org/apache/spark/repl/carbon/Main.scala
@@ -1,7 +1,3 @@
-package org.apache.spark.repl.carbon
-
-import org.apache.spark.repl.{CarbonSparkILoop, SparkILoop}
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -18,6 +14,11 @@ import org.apache.spark.repl.{CarbonSparkILoop, SparkILoop}
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.spark.repl.carbon
+
+import org.apache.spark.repl.{CarbonSparkILoop, SparkILoop}
+
 object Main {
 
     private var _interp: SparkILoop = _
