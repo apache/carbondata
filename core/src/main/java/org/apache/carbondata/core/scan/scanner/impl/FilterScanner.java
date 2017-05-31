d3758d80a6856295f1aec61150fc6ac9ae1b9b98
mayun <simafengyun1984@163.com>
2017-05-31 18:18:35 +0800

    Revert "put if clause out of for clause"
    This reverts commit 497c8de1be16d72375e6b6af81b099ef35c092fb.

--- core/src/main/java/org/apache/carbondata/core/scan/scanner/impl/FilterScanner.java
+++ core/src/main/java/org/apache/carbondata/core/scan/scanner/impl/FilterScanner.java
@@ -282,15 +282,15 @@ public class FilterScanner extends AbstractBlockletScanner {
     MeasureColumnDataChunk[][] measureColumnDataChunks =
         new MeasureColumnDataChunk[measureRawColumnChunks.length][indexesGroup.length];
     for (int i = 0; i < dimensionRawColumnChunks.length; i++) {
-      if (dimensionRawColumnChunks[i] != null) {
-        for (int j = 0; j < indexesGroup.length; j++) {
+      for (int j = 0; j < indexesGroup.length; j++) {
+        if (dimensionRawColumnChunks[i] != null) {
           dimensionColumnDataChunks[i][j] = dimensionRawColumnChunks[i].convertToDimColDataChunk(j);
         }
       }
     }
     for (int i = 0; i < measureRawColumnChunks.length; i++) {
-      if (measureRawColumnChunks[i] != null) {
-        for (int j = 0; j < indexesGroup.length; j++) {
+      for (int j = 0; j < indexesGroup.length; j++) {
+        if (measureRawColumnChunks[i] != null) {
           measureColumnDataChunks[i][j] = measureRawColumnChunks[i].convertToMeasureColDataChunk(j);
         }
       }
