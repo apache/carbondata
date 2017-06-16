f22a0da4f9bdf74e36207fad8dc941b191daae3c
mayun <simafengyun1984@163.com>
2017-06-16 21:42:28 +0800

    change test code

--- integration/spark-common-test/src/test/scala/org/apache/carbondata/spark/testsuite/partition/TestShowPartitions.scala
+++ integration/spark-common-test/src/test/scala/org/apache/carbondata/spark/testsuite/partition/TestShowPartitions.scala
@@ -95,7 +95,9 @@ class TestShowPartition  extends QueryTest with BeforeAndAfterAll {
   }
   test("show partition table: not default db") {
 
-    sql(s"CREATE DATABASE IF NOT EXISTS partitionDB")
+    sql("drop table if exists partitionDB.listTable")
+    sql(s"DROP DATABASE partitionDB")
+    sql(s"CREATE DATABASE partitionDB")
 
     sql(
       """
@@ -125,6 +127,6 @@ class TestShowPartition  extends QueryTest with BeforeAndAfterAll {
     sql("drop table if exists rangeTable")
     sql("drop table if exists listTable")
     sql("drop table if exists partitionDB.listTable")
-    sql(s"DROP DATABASE if exists partitionDB")
+    sql(s"DROP DATABASE partitionDB")
   }
 }
