61f0c482e986f0c3cd5966c9f74a92f1277a49e0
QiangCai <qiangcai@qq.com>
2017-08-24 12:31:44 +0800

    fix default end key for non-dictionary

--- core/src/main/java/org/apache/carbondata/core/scan/filter/FilterUtil.java
+++ core/src/main/java/org/apache/carbondata/core/scan/filter/FilterUtil.java
@@ -1205,7 +1205,7 @@ public final class FilterUtil {
       startPoint++;
     }
     for (int i = 0; i < numberOfNoDictionaryDimension; i++) {
-      noDictionaryEndKeyBuffer.put((byte) 127);
+      noDictionaryEndKeyBuffer.put((byte) 0xFF);
     }
     return noDictionaryEndKeyBuffer.array();
   }
@@ -1460,7 +1460,7 @@ public final class FilterUtil {
         continue;
       }
       if (null == setOfStartKeyByteArray.get(dimension.getOrdinal())) {
-        setOfStartKeyByteArray.put(dimension.getOrdinal(), new byte[] { 127 });
+        setOfStartKeyByteArray.put(dimension.getOrdinal(), new byte[] { (byte) 0xFF });
       }
 
     }
@@ -1492,7 +1492,7 @@ public final class FilterUtil {
       if (CarbonUtil.hasEncoding(dimension.getEncoder(), Encoding.DICTIONARY)) {
         continue;
       }
-      setOfEndKeyByteArray.put(dimension.getOrdinal(), new byte[] { 127 });
+      setOfEndKeyByteArray.put(dimension.getOrdinal(), new byte[] { (byte) 0xFF });
     }
   }
 
