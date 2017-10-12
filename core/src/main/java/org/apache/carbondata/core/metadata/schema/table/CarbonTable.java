33b920c4f8b9ad5f465044e76baeb17a6abdc9d2
Jacky Li <jacky.likun@qq.com>
2017-10-12 14:44:52 +0800

    fix comment

--- core/src/main/java/org/apache/carbondata/core/metadata/schema/table/CarbonTable.java
+++ core/src/main/java/org/apache/carbondata/core/metadata/schema/table/CarbonTable.java
@@ -18,13 +18,7 @@
 package org.apache.carbondata.core.metadata.schema.table;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.apache.carbondata.core.constants.CarbonCommonConstants;
 import org.apache.carbondata.core.metadata.AbsoluteTableIdentifier;
