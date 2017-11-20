6ec7d405cef2b6ea69d467e7f678195063dc126c
simon.xu <simon.xubo@huawei.com>
2017-11-20 10:10:01 +0800

    save this work before change target branch

--- datamap/lucene/src/test/scala/org/apache/carbondata/datamap/lucene/LuceneDataMapScalaTest.scala
+++ datamap/lucene/src/test/scala/org/apache/carbondata/datamap/lucene/LuceneDataMapScalaTest.scala
@@ -55,9 +55,15 @@ class LuceneDataMapScalaTest extends TestCase  with AssertionsForJUnit{
 
   @Test def testLuceneDatamap(): Unit = {
 
-    carbonSession.sql("use default")
+    carbonSession.sql("drop table if exists test_table")
     carbonSession
       .sql("create table if not exists test_table(id long, name string, city string, age int) stored by 'carbondata'")
+
+
+    carbonSession.sql("create datamap lucenedatamap on table test_table using" +
+    " 'org.apache.carbondata.datamap.lucene.LuceneDataMapFactory' " +
+    " dmproperties() as select * from test_table")
+
     val options = Map("tableName" -> "test_table")
     val rdd = carbonSession.sparkContext.makeRDD(
       Seq((1, "david", "shenzhen", 31),
@@ -66,60 +72,51 @@ class LuceneDataMapScalaTest extends TestCase  with AssertionsForJUnit{
         (4, "simon", "guangzhou", 39)))
     val df = carbonSession.createDataFrame(rdd);
 
-    val dataMapStoreManager = DataMapStoreManager.getInstance();
     val dbName = "default"
     val tableName = "test_table"
     val tableIndentifier = AbsoluteTableIdentifier.from(storePath, dbName, tableName)
 
     df.write.format("carbondata").options(options).mode(SaveMode.Overwrite).save
-
-    dataMapStoreManager.createAndRegisterDataMap(tableIndentifier,
-      classOf[LuceneDataMapFactory].getName, LuceneDataMap.NAME)
-
-    DataMapStoreManager.setClearData(false)
-
-    df.write.format("carbondata").options(options).mode(SaveMode.Overwrite).save
-
-    val lstTableDataMap = dataMapStoreManager.
-      getDataMap(tableIndentifier, LuceneDataMap.NAME, classOf[LuceneDataMapFactory].getName)
-
-    assert(lstTableDataMap != null, "list table data map is empty")
-
-    val Writer = lstTableDataMap.getDataMapFactory.createWriter("0")
-    val luceneWriter = classOf[LuceneDataMapWriter].cast(Writer)
-    val lucenePath = luceneWriter.getIndexPath("0");
-
-    assert(new File(lucenePath).exists(), "lucene data not exists");
-
-    /**
-      *  query lucene data
-      */
-    val indexDir = FSDirectory.open(Paths.get(lucenePath))
-    val indexSearcher = new IndexSearcher(DirectoryReader.open(indexDir))
-    val queryParser = new MultiFieldQueryParser(Array("id","name","city","age"),new StandardAnalyzer())
-    val query = queryParser.parse("*:*")
-    val search = indexSearcher.search(query,1000);
-
-    /**
-      * assert those results
-      */
-    assertResult(4)(search.scoreDocs.length)
-
-    search.scoreDocs.foreach(scoreDoc =>{
-      val doc = indexSearcher.doc(scoreDoc.doc)
-      assertResult(3)(doc.getFields.size())
-    })
-
-    val result = carbonSession.sql("select * from test_table where _3 like 'wuha%'").collect()
-    assertResult(1)(result.length
-    //ßassertResult(Seq("wuhan"))(dfResult.select("_3").rdd.take(1)(0))
-
-    DataMapStoreManager.setClearData(true)
+//
+//    val dataMapStoreManager = DataMapStoreManager.getInstance();
+//    val lstTableDataMap = dataMapStoreManager.
+//      getDataMap(tableIndentifier, LuceneDataMap.NAME, classOf[LuceneDataMapFactory].getName)
+//
+//    assert(lstTableDataMap != null, "list table data map is empty")
+//
+//    val Writer = lstTableDataMap.getDataMapFactory.createWriter("0")
+//    val luceneWriter = classOf[LuceneDataMapWriter].cast(Writer)
+//    val lucenePath = luceneWriter.getIndexPath("0");
+//
+//    assert(new File(lucenePath).exists(), s"lucene ${lucenePath} data not exists");
+//
+//    /**
+//      *  query lucene data
+//      */
+//    val indexDir = FSDirectory.open(Paths.get(lucenePath))
+//    val indexSearcher = new IndexSearcher(DirectoryReader.open(indexDir))
+//    val queryParser = new MultiFieldQueryParser(Array("id","name","city","age"),new StandardAnalyzer())
+//    val query = queryParser.parse("*:*")
+//    val search = indexSearcher.search(query,1000);
+//
+//    /**
+//      * assert those results
+//      */
+//    assertResult(4)(search.scoreDocs.length)
+//
+//    search.scoreDocs.foreach(scoreDoc =>{
+//      val doc = indexSearcher.doc(scoreDoc.doc)
+//      assertResult(3)(doc.getFields.size())
+//    })
+//
+//    val result = carbonSession.sql("select * from test_table where _3 like 'wuha%'").collect()
+//    assertResult(1)(result.length)
+//    assertResult(Row.fromSeq(Seq(3,"jarry","wuhan",35)))(result.apply(0))
 
   }
 
   @AfterClass override def tearDown(): Unit = {
-    carbonSession.stop()
+    //carbonSession.stop()
     //carbonSession.close()
     //deleteDirectory(new File(rootPath))
   }
