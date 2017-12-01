62dd2cb3c70f8555cb625dd89a59233d103ad08b
Geetika Gupta <geetika.gupta@knoldus.in>
2017-12-01 13:49:32 +0530

    1. Added validation for table properties 2. Added related test cases 3. Fixed test cases

--- integration/spark2/src/main/scala/org/apache/spark/sql/parser/CarbonSparkSqlParser.scala
+++ integration/spark2/src/main/scala/org/apache/spark/sql/parser/CarbonSparkSqlParser.scala
@@ -257,6 +257,30 @@ class CarbonHelperSqlAstBuilder(conf: SQLConf, parser: CarbonSpark2SqlParser)
     tblProperties
   }
 
+  private def validateTableProperties(properties: Map[String, String]): Map[String, String] = {
+    var isSupported = true
+    val invalidOptions = StringBuilder.newBuilder
+    val tableProperties = Seq("DICTIONARY_INCLUDE", "DICTIONARY_EXCLUDE", "NO_INVERTED_INDEX",
+      "SORT_COLUMNS", "TABLE_BLOCKSIZE", "STREAMING", "SORT_SCOPE", "COMMENT", "PARTITION_TYPE",
+      "NUM_PARTITIONS", "RANGE_INFO", "LIST_INFO", "BUCKETNUMBER", "BUCKETCOLUMNS")
+    val tblProperties: Map[String, String] = properties.filter { property =>
+      if (!(tableProperties.exists(prop => prop.equalsIgnoreCase(property._1))
+            || property._1.toUpperCase.startsWith("DEFAULT.VALUE")
+            || property._1.toUpperCase.startsWith("COLUMNPROPERTIES"))) {
+      isSupported = false
+      invalidOptions.append(property._1)
+        false
+    } else {
+      true
+    }
+    }
+    if (!isSupported) {
+      val errorMessage = "Error: Invalid option(s): " + invalidOptions.toString()
+      throw new MalformedCarbonCommandException(errorMessage)
+    }
+    tblProperties
+  }
+
   private def validateStreamingProperty(carbonOption: CarbonOption): Unit = {
     try {
       carbonOption.isStreaming
