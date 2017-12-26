1937a21a210c16c4a185a803515b6932d0bc0553
dhatchayani <dhatcha.official@gmail.com>
2017-12-26 11:40:14 +0530

    [CARBONDATA-1939] Added show segments validation test case
    1. Modified headers of show segments
    
    2. Modified SDV test cases for validating headers and result
    
    This closes #1723

--- integration/spark-common-cluster-test/src/test/scala/org/apache/carbondata/cluster/sdv/generated/ShowLoadsTestCase.scala
+++ integration/spark-common-cluster-test/src/test/scala/org/apache/carbondata/cluster/sdv/generated/ShowLoadsTestCase.scala
@@ -36,7 +36,21 @@ class ShowLoadsTestCase extends QueryTest with BeforeAndAfterAll {
   sql(s"""CREATE TABLE ShowSegment_196 (CUST_ID int,CUST_NAME String,ACTIVE_EMUI_VERSION string,DOB timestamp, DOJ timestamp, BIGINT_COLUMN1 bigint,BIGINT_COLUMN2 bigint,DECIMAL_COLUMN1 decimal(30,10),Double_COLUMN1 double,DECIMAL_COLUMN2 decimal(36,10), Double_COLUMN2 double,INTEGER_COLUMN1 int) STORED BY 'org.apache.carbondata.format' TBLPROPERTIES('table_blocksize'='1')""").collect
   sql(s"""LOAD DATA INPATH '$resourcesPath/Data/InsertData/join1.csv' into table ShowSegment_196 OPTIONS('DELIMITER'=',' , 'QUOTECHAR'='"','BAD_RECORDS_ACTION'='FORCE','FILEHEADER'='CUST_ID,CUST_NAME,ACTIVE_EMUI_VERSION,DOB,DOJ,BIGINT_COLUMN1,BIGINT_COLUMN2,DECIMAL_COLUMN1,Double_COLUMN1,DECIMAL_COLUMN2,Double_COLUMN2,INTEGER_COLUMN1')""").collect
   sql(s"""LOAD DATA INPATH '$resourcesPath/Data/InsertData/join1.csv' into table ShowSegment_196 OPTIONS('DELIMITER'=',' , 'QUOTECHAR'='"','BAD_RECORDS_ACTION'='FORCE','FILEHEADER'='CUST_ID,CUST_NAME,ACTIVE_EMUI_VERSION,DOB,DOJ,BIGINT_COLUMN1,BIGINT_COLUMN2,DECIMAL_COLUMN1,Double_COLUMN1,DECIMAL_COLUMN2,Double_COLUMN2,INTEGER_COLUMN1')""").collect
-  sql(s"""show segments for table ShowSegment_196""").collect()
+
+  val df = sql(s"""show segments for table ShowSegment_196""").collect()
+  // validating headers
+  val header = df(0).schema
+   assert(header(0).name.equalsIgnoreCase("SegmentSequenceId"))
+   assert(header(1).name.equalsIgnoreCase("Status"))
+   assert(header(2).name.equalsIgnoreCase("Load Start Time"))
+   assert(header(3).name.equalsIgnoreCase("Load End Time"))
+   assert(header(4).name.equalsIgnoreCase("Merged To"))
+   assert(header(5).name.equalsIgnoreCase("File Format"))
+   val col = df.map {
+     row => Row(row.getString(0), row.getString(1), row.getString(4))
+   }.toSeq
+   assert(col.equals(Seq(Row("1", "Success", "NA"),
+     Row("0", "Success", "NA"))))
     sql(s"""drop table ShowSegment_196""").collect
  }
 
@@ -46,7 +60,11 @@ class ShowLoadsTestCase extends QueryTest with BeforeAndAfterAll {
     sql(s"""drop TABLE if exists Database_ShowSegment_196""").collect
   sql(s"""CREATE TABLE Database_ShowSegment_196 (CUST_ID int,CUST_NAME String,ACTIVE_EMUI_VERSION string,DOB timestamp, DOJ timestamp, BIGINT_COLUMN1 bigint,BIGINT_COLUMN2 bigint,DECIMAL_COLUMN1 decimal(30,10),Double_COLUMN1 double,DECIMAL_COLUMN2 decimal(36,10), Double_COLUMN2 double,INTEGER_COLUMN1 int) STORED BY 'org.apache.carbondata.format' TBLPROPERTIES('table_blocksize'='1')""").collect
   sql(s"""LOAD DATA INPATH '$resourcesPath/Data/InsertData/join1.csv' into table Database_ShowSegment_196 OPTIONS('DELIMITER'=',' , 'QUOTECHAR'='"','BAD_RECORDS_ACTION'='FORCE','FILEHEADER'='CUST_ID,CUST_NAME,ACTIVE_EMUI_VERSION,DOB,DOJ,BIGINT_COLUMN1,BIGINT_COLUMN2,DECIMAL_COLUMN1,Double_COLUMN1,DECIMAL_COLUMN2,Double_COLUMN2,INTEGER_COLUMN1')""").collect
-  sql(s"""show segments for table default.Database_ShowSegment_196""").collect()
+  val df = sql(s"""show segments for table default.Database_ShowSegment_196""").collect()
+   val col = df.map {
+     row => Row(row.getString(0), row.getString(1), row.getString(4))
+   }.toSeq
+   assert(col.equals(Seq(Row("0", "Success", "NA"))))
     sql(s"""drop table Database_ShowSegment_196""").collect
  }
 
@@ -55,8 +73,11 @@ class ShowLoadsTestCase extends QueryTest with BeforeAndAfterAll {
  test("DataLoadManagement001_830", Include) {
     sql(s"""drop TABLE if exists Case_ShowSegment_196""").collect
   sql(s"""CREATE TABLE Case_ShowSegment_196 (CUST_ID int,CUST_NAME String,ACTIVE_EMUI_VERSION string,DOB timestamp, DOJ timestamp, BIGINT_COLUMN1 bigint,BIGINT_COLUMN2 bigint,DECIMAL_COLUMN1 decimal(30,10),Double_COLUMN1 double,DECIMAL_COLUMN2 decimal(36,10), Double_COLUMN2 double,INTEGER_COLUMN1 int) STORED BY 'org.apache.carbondata.format' TBLPROPERTIES('table_blocksize'='1')""").collect
-   sql(s"""show segments for table CASE_ShowSegment_196""").collect
-
+   val df = sql(s"""show segments for table CASE_ShowSegment_196""").collect()
+   val col = df.map {
+     row => Row(row.getString(0), row.getString(1), row.getString(4))
+   }.toSeq
+   assert(col.equals(Seq()))
     sql(s"""drop table Case_ShowSegment_196""").collect
  }
 
