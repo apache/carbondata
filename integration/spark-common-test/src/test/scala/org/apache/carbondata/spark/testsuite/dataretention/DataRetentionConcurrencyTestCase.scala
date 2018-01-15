4adb87a7dcbcd9c4dc4d32a28c73f2a4ba7bf4ae
anubhav100 <anubhav.tarar@knoldus.in>
2018-01-15 12:36:58 +0530

    [HOTFIX] [CARBONDATA-2027] Fix the Failing Concurrent Test cases for CI
    This closes #1801

--- integration/spark-common-test/src/test/scala/org/apache/carbondata/spark/testsuite/dataretention/DataRetentionConcurrencyTestCase.scala
+++ integration/spark-common-test/src/test/scala/org/apache/carbondata/spark/testsuite/dataretention/DataRetentionConcurrencyTestCase.scala
@@ -18,13 +18,16 @@
 package org.apache.carbondata.spark.testsuite.dataretention
 
 import java.util
-import java.util.concurrent.{Callable, Executors}
+import java.util.concurrent.{Callable, Executors, Future}
 
-import org.scalatest.{BeforeAndAfterAll, Ignore}
+import scala.collection.JavaConverters._
+
+import org.apache.spark.sql.test.util.QueryTest
+import org.scalatest.BeforeAndAfterAll
 
 import org.apache.carbondata.core.constants.CarbonCommonConstants
 import org.apache.carbondata.core.util.CarbonProperties
-import org.apache.spark.sql.test.util.QueryTest
+
 
 /**
  * This class contains DataRetention concurrency test cases
@@ -34,7 +37,8 @@ class DataRetentionConcurrencyTestCase extends QueryTest with BeforeAndAfterAll 
   private val executorService = Executors.newFixedThreadPool(10)
 
   override def beforeAll {
-    CarbonProperties.getInstance().addProperty(CarbonCommonConstants.MAX_TIMEOUT_FOR_CONCURRENT_LOCK, "1")
+    CarbonProperties.getInstance()
+      .addProperty(CarbonCommonConstants.MAX_TIMEOUT_FOR_CONCURRENT_LOCK, "1")
     sql("drop table if exists concurrent")
     sql(
       "create table concurrent (ID int, date String, country String, name " +
@@ -56,13 +60,14 @@ class DataRetentionConcurrencyTestCase extends QueryTest with BeforeAndAfterAll 
 
     val tasks = new util.ArrayList[Callable[String]]()
     tasks
-      .add(new QueryTask(s"LOAD DATA LOCAL INPATH '$resourcesPath/dataretention1.csv' INTO TABLE concurrent OPTIONS('DELIMITER' =  ',')"))
+      .add(new QueryTask(s"LOAD DATA LOCAL INPATH '$resourcesPath/dataretention1.csv' INTO TABLE " +
+                         s"concurrent OPTIONS('DELIMITER' =  ',')"))
     tasks.add(new QueryTask("delete from table concurrent where segment.id in (0)"))
     tasks.add(new QueryTask("clean files for table concurrent"))
-    val results = executorService.invokeAll(tasks)
-    for (i <- 0 until tasks.size()) {
-      val res = results.get(i).get
-      assert("PASS".equals(res))
+    val futures = executorService.invokeAll(tasks)
+    val results = futures.asScala.map(_.get)
+    for (i <- results.indices) {
+      assert("PASS".equals(results(i)))
     }
   }
 
@@ -74,15 +79,18 @@ class DataRetentionConcurrencyTestCase extends QueryTest with BeforeAndAfterAll 
 
     val tasks = new util.ArrayList[Callable[String]]()
     tasks
-      .add(new QueryTask(s"LOAD DATA LOCAL INPATH '$resourcesPath/dataretention1.csv' INTO TABLE concurrent OPTIONS('DELIMITER' =  ',')"))
+      .add(new QueryTask(s"LOAD DATA LOCAL INPATH '$resourcesPath/dataretention1.csv' INTO TABLE " +
+                         s"concurrent OPTIONS('DELIMITER' =  ',')"))
     tasks
       .add(new QueryTask(
         "delete from table concurrent where segment.starttime before '2099-01-01 00:00:00'"))
     tasks.add(new QueryTask("clean files for table concurrent"))
-    val results = executorService.invokeAll(tasks)
-    for (i <- 0 until tasks.size()) {
-      val res = results.get(i).get
-      assert("PASS".equals(res))
+
+    val futures: util.List[Future[String]] = executorService.invokeAll(tasks)
+
+    val results = futures.asScala.map(_.get)
+    for (i <- results.indices) {
+      assert("PASS".equals(results(i)))
     }
   }
 
