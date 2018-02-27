1c47034268dd5eaac15515009e6512671d6fad29
Jacky Li <jacky.likun@qq.com>
2018-02-27 19:07:11 +0800

    fix style

--- integration/spark2/src/main/scala/org/apache/spark/sql/execution/strategy/CarbonLateDecodeStrategy.scala
+++ integration/spark2/src/main/scala/org/apache/spark/sql/execution/strategy/CarbonLateDecodeStrategy.scala
@@ -527,7 +527,8 @@ private[sql] class CarbonLateDecodeStrategy extends SparkStrategy {
     predicate match {
       case u: ScalaUDF if u.function.isInstanceOf[TextMatchUDF] =>
         if (u.children.size > 1) {
-          throw new MalformedCarbonCommandException("TEXT_MATCH UDF syntax: TEXT_MATCH('luceneQuerySyntax')")
+          throw new MalformedCarbonCommandException(
+            "TEXT_MATCH UDF syntax: TEXT_MATCH('luceneQuerySyntax')")
         }
         Some(TextMatch(u.children.head.toString()))
 
