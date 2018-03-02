488d3c09e481c00126655573a58ab79927545467
chenliang613 <chenliang613@huawei.com>
2018-03-02 19:37:06 +0800

    fix code style issue

--- examples/spark2/src/main/scala/org/apache/carbondata/examples/StandardPartitionExample.scala
+++ examples/spark2/src/main/scala/org/apache/carbondata/examples/StandardPartitionExample.scala
@@ -19,9 +19,10 @@ package org.apache.carbondata.examples
 
 import java.io.File
 
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
 import org.apache.carbondata.core.constants.CarbonCommonConstants
 import org.apache.carbondata.core.util.CarbonProperties
-import org.apache.spark.sql.{SaveMode, SparkSession}
 
 /**
  * This example is for standard partition, same as hive and spark partition
