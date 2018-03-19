67b46151d3287fddae1b7d8da68c6032fe10620a
root <601450868@qq.com>
2018-03-19 19:59:26 +0800

    [CARBONDATA-2262] Support the syntax of 'STORED AS CARBONDATA' to create table
    fix scalastyle
    
    fix spark2.1 error
    
    optimize
    
    optimize
    
    optimize according review comment
    
    optimize

--- integration/spark2/src/main/spark2.2/org/apache/spark/sql/hive/CarbonSessionState.scala
+++ integration/spark2/src/main/spark2.2/org/apache/spark/sql/hive/CarbonSessionState.scala
@@ -325,6 +325,7 @@ class CarbonSqlAstBuilder(conf: SQLConf, parser: CarbonSpark2SqlParser, sparkSes
     val fileStorage = helper.getFileStorage(ctx.createFileFormat)
 
     if (fileStorage.equalsIgnoreCase("'carbondata'") ||
+        fileStorage.equalsIgnoreCase("carbondata") ||
         fileStorage.equalsIgnoreCase("'carbonfile'") ||
         fileStorage.equalsIgnoreCase("'org.apache.carbondata.format'")) {
       val createTableTuple = (ctx.createTableHeader, ctx.skewSpec,
