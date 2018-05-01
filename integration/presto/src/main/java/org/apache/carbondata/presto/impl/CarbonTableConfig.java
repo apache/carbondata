9706f59df0f27769c6d3416d2daa4bb2d572e134
anubhav100 <anubhav.tarar@knoldus.in>
2018-05-01 09:59:05 +0530

    Fixed Presto Can't Query CarbonData When CarbonStore is at s3

--- integration/presto/src/main/java/org/apache/carbondata/presto/impl/CarbonTableConfig.java
+++ integration/presto/src/main/java/org/apache/carbondata/presto/impl/CarbonTableConfig.java
@@ -17,9 +17,9 @@
 
 package org.apache.carbondata.presto.impl;
 
-import io.airlift.configuration.Config;
-
 import javax.validation.constraints.NotNull;
+
+import io.airlift.configuration.Config;
 
 /**
  * Configuration read from etc/catalog/carbondata.properties
@@ -32,6 +32,14 @@ public class CarbonTableConfig {
   private String storePath;
   private String unsafeMemoryInMb;
   private String enableUnsafeInQueryExecution;
+  private String s3A_acesssKey;
+  private String s3A_secretKey;
+  private String s3_acesssKey;
+  private String s3_secretKey;
+  private String s3N_acesssKey;
+  private String s3N_secretKey;
+  private String endPoint;
+
 
   @NotNull public String getDbPath() {
     return dbPath;
@@ -51,7 +59,8 @@ public class CarbonTableConfig {
     return this;
   }
 
-  @NotNull public String getStorePath() {
+  @NotNull public String
+  getStorePath() {
     return storePath;
   }
 
@@ -59,6 +68,7 @@ public class CarbonTableConfig {
     this.storePath = storePath;
     return this;
   }
+
 
   public String getUnsafeMemoryInMb() {
     return unsafeMemoryInMb;
@@ -77,6 +87,75 @@ public class CarbonTableConfig {
   @Config("enable.unsafe.in.query.processing")
   public CarbonTableConfig setEnableUnsafeInQueryExecution(String enableUnsafeInQueryExecution) {
     this.enableUnsafeInQueryExecution = enableUnsafeInQueryExecution;
+    return this;
+  }
+
+  public String getS3A_AcesssKey() {
+    return s3A_acesssKey;
+  }
+
+  public String getS3A_SecretKey() {
+    return s3A_secretKey;
+  }
+
+  public String getS3_AcesssKey() {
+    return s3_acesssKey;
+  }
+
+  public String getS3_SecretKey() {
+    return s3_secretKey;
+  }
+
+  public String getS3N_AcesssKey() {
+    return s3N_acesssKey;
+  }
+
+  public String getS3N_SecretKey() {
+    return s3N_secretKey;
+  }
+
+  public String getS3EndPoint() {
+    return endPoint;
+  }
+
+
+  @Config("fs.s3a.access.key")
+  public CarbonTableConfig setS3A_AcesssKey(String s3A_acesssKey) {
+    this.s3A_acesssKey = s3A_acesssKey;
+    return this;
+  }
+
+  @Config("fs.s3a.secret.key")
+  public CarbonTableConfig setS3A_SecretKey(String s3A_secretKey) {
+    this.s3A_secretKey = s3A_secretKey;
+    return this;
+  }
+
+  @Config("fs.s3.awsAccessKeyId")
+  public CarbonTableConfig setS3_AcesssKey(String s3_acesssKey) {
+    this.s3_acesssKey = s3_acesssKey;
+    return this;
+  }
+
+  @Config("fs.s3.awsSecretAccessKey")
+  public CarbonTableConfig setS3_SecretKey(String s3_secretKey) {
+    this.s3_secretKey = s3_secretKey;
+    return this;
+  }
+  @Config("fs.s3n.awsAccessKeyId")
+  public CarbonTableConfig setS3N_AcesssKey(String s3N_acesssKey) {
+    this.s3N_acesssKey = s3N_acesssKey;
+    return this;
+  }
+
+  @Config("fs.s3.awsSecretAccessKey")
+  public CarbonTableConfig setS3N_SecretKey(String s3N_secretKey) {
+    this.s3N_secretKey = s3N_secretKey;
+    return this;
+  }
+  @Config("fs.s3a.endpoint")
+  public CarbonTableConfig setS3EndPoint(String endPoint) {
+    this.endPoint = endPoint;
     return this;
   }
 }
