ddf3e85952d066cb82f4963c5f7bce6a8196f095
xubo245 <xubo29@huawei.com>
2018-05-23 21:45:49 +0800

    [CARBONDATA-2519] Add document for CarbonReader
    Add document for CarbonReader
    
    This closes #2337

--- store/sdk/src/main/java/org/apache/carbondata/sdk/file/CarbonReaderBuilder.java
+++ store/sdk/src/main/java/org/apache/carbondata/sdk/file/CarbonReaderBuilder.java
@@ -50,26 +50,53 @@ public class CarbonReaderBuilder {
   private String tableName;
   private boolean isTransactionalTable = true;
 
+  /**
+   * Construct a CarbonReaderBuilder with table path and table name
+   *
+   * @param tablePath table path
+   * @param tableName table name
+   */
   CarbonReaderBuilder(String tablePath, String tableName) {
     this.tablePath = tablePath;
     this.tableName = tableName;
   }
 
+  /**
+   * Configure the projection column names of carbon reader
+   *
+   * @param projectionColumnNames projection column names
+   * @return CarbonReaderBuilder object
+   */
   public CarbonReaderBuilder projection(String[] projectionColumnNames) {
     Objects.requireNonNull(projectionColumnNames);
     this.projectionColumns = projectionColumnNames;
     return this;
   }
 
+  /**
+   * Configure the transactional status of table
+   * If set to false, then reads the carbondata and carbonindex files from a flat folder structure.
+   * If set to true, then reads the carbondata and carbonindex files from segment folder structure.
+   * Default value is true
+   *
+   * @param isTransactionalTable whether is transactional table or not
+   * @return CarbonReaderBuilder object
+   */
   public CarbonReaderBuilder isTransactionalTable(boolean isTransactionalTable) {
     Objects.requireNonNull(isTransactionalTable);
     this.isTransactionalTable = isTransactionalTable;
     return this;
   }
 
-  public CarbonReaderBuilder filter(Expression fileterExpression) {
-    Objects.requireNonNull(fileterExpression);
-    this.filterExpression = fileterExpression;
+  /**
+   * Configure the filter expression for carbon reader
+   *
+   * @param filterExpression filter expression
+   * @return CarbonReaderBuilder object
+   */
+  public CarbonReaderBuilder filter(Expression filterExpression) {
+    Objects.requireNonNull(filterExpression);
+    this.filterExpression = filterExpression;
     return this;
   }
 
@@ -78,7 +105,7 @@ public class CarbonReaderBuilder {
    *
    * @param key   the string of access key for different S3 type,like: fs.s3a.access.key
    * @param value the value of access key
-   * @return CarbonWriterBuilder
+   * @return CarbonWriterBuilder object
    */
   public CarbonReaderBuilder setAccessKey(String key, String value) {
     FileFactory.getConfiguration().set(key, value);
@@ -89,7 +116,7 @@ public class CarbonReaderBuilder {
    * Set the access key for S3.
    *
    * @param value the value of access key
-   * @return CarbonWriterBuilder
+   * @return CarbonWriterBuilder object
    */
   public CarbonReaderBuilder setAccessKey(String value) {
     return setAccessKey(Constants.ACCESS_KEY, value);
@@ -100,7 +127,7 @@ public class CarbonReaderBuilder {
    *
    * @param key   the string of secret key for different S3 type,like: fs.s3a.secret.key
    * @param value the value of secret key
-   * @return CarbonWriterBuilder
+   * @return CarbonWriterBuilder object
    */
   public CarbonReaderBuilder setSecretKey(String key, String value) {
     FileFactory.getConfiguration().set(key, value);
@@ -111,7 +138,7 @@ public class CarbonReaderBuilder {
    * Set the secret key for S3
    *
    * @param value the value of secret key
-   * @return CarbonWriterBuilder
+   * @return CarbonWriterBuilder object
    */
   public CarbonReaderBuilder setSecretKey(String value) {
     return setSecretKey(Constants.SECRET_KEY, value);
@@ -122,7 +149,7 @@ public class CarbonReaderBuilder {
    *
    * @param key   the string of endpoint for different S3 type,like: fs.s3a.endpoint
    * @param value the value of endpoint
-   * @return CarbonWriterBuilder
+   * @return CarbonWriterBuilder object
    */
   public CarbonReaderBuilder setEndPoint(String key, String value) {
     FileFactory.getConfiguration().set(key, value);
@@ -133,13 +160,20 @@ public class CarbonReaderBuilder {
    * Set the endpoint for S3
    *
    * @param value the value of endpoint
-   * @return CarbonWriterBuilder
+   * @return CarbonWriterBuilder object
    */
   public CarbonReaderBuilder setEndPoint(String value) {
-    FileFactory.getConfiguration().set(Constants.ENDPOINT, value);
-    return this;
+    return setEndPoint(Constants.ENDPOINT, value);
   }
 
+  /**
+   * Build CarbonReader
+   *
+   * @param <T>
+   * @return CarbonReader
+   * @throws IOException
+   * @throws InterruptedException
+   */
   public <T> CarbonReader<T> build() throws IOException, InterruptedException {
     CarbonTable table = CarbonTable.buildFromTablePath(tableName, tablePath, isTransactionalTable);
 
