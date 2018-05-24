2b8ae2628d50efcd095696b5bf614eab2fcdb8d2
sujith71955 <sujithchacko.2010@gmail.com>
2018-05-24 11:21:50 +0530

    [CARBONDATA-2532][Integration] Carbon to support spark 2.3 version, ColumnVector Interface
    Column vector and Columnar Batch interface compatibility issues has been
    addressed in this PR, The changes were related to below modifications
    done in spark interface
    a) This is a refactoring of ColumnVector hierarchy and related classes.
    b) make ColumnVector read-only
    c) introduce WritableColumnVector with write interface
    d) remove ReadOnlyColumnVector
    
    In this PR inorder to hide the compatibility issues of columnar vector
    API's from the existing common classes, i introduced an interface of the
    proxy vector readers, this
    proxy vector readers will take care the compatibility issues with
    respect to spark different versions.

---
+++ integration/spark2/src/main/spark2.3/org/apache/spark/sql/ColumnVectorFactory.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.sql;
+
+import org.apache.spark.memory.MemoryMode;
+import org.apache.spark.sql.execution.vectorized.OnHeapColumnVector;
+import org.apache.spark.sql.execution.vectorized.OffHeapColumnVector;
+import org.apache.spark.sql.execution.vectorized.WritableColumnVector;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.vectorized.ColumnarBatch;
+
+public class ColumnVectorFactory {
+
+
+    public static WritableColumnVector[] getColumnVector(MemoryMode memMode, StructType outputSchema, int rowNums) {
+
+
+        WritableColumnVector[] writableColumnVectors = null;
+        switch (memMode) {
+            case ON_HEAP:
+                writableColumnVectors = OnHeapColumnVector
+                        .allocateColumns(rowNums, outputSchema);
+                break;
+            case OFF_HEAP:
+                writableColumnVectors = OffHeapColumnVector
+                        .allocateColumns(rowNums, outputSchema);
+                break;
+        }
+        return writableColumnVectors;
+    }
+}
