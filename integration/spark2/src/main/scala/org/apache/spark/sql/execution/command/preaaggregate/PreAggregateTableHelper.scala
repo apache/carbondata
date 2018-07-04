6f48ccb357505c958ae7f73ced99049ebeb0ec5b
Manhua <kevinjmh@qq.com>
2018-07-04 15:02:25 +0800

    fix comment and code style

--- integration/spark2/src/main/scala/org/apache/spark/sql/execution/command/preaaggregate/PreAggregateTableHelper.scala
+++ integration/spark2/src/main/scala/org/apache/spark/sql/execution/command/preaaggregate/PreAggregateTableHelper.scala
@@ -107,11 +107,13 @@ case class PreAggregateTableHelper(
       parentTable.getTableInfo.getFactTable.getTableProperties.asScala.getOrElse(
         CarbonCommonConstants.FLAT_FOLDER, CarbonCommonConstants.DEFAULT_FLAT_FOLDER))
 
-    // update column name with prefix for long_string_column
+    // Datamap table name and columns are automatically added prefix with parent table name
+    // in carbon. For convenient, users can type column names same as the ones in select statement
+    // when config dmproperties, and here we update column names with prefix.
     val longStringColumn = tableProperties.get(CarbonCommonConstants.LONG_STRING_COLUMNS)
     if (longStringColumn != None) {
       val fieldNames = fields.map(_.column)
-      val newLongStringColumn = longStringColumn.get.split(",").map(_.trim).map(colName => {
+      val newLongStringColumn = longStringColumn.get.split(",").map(_.trim).map{ colName =>
         val newColName = parentTable.getTableName.toLowerCase() + "_" + colName
         if (!fieldNames.contains(newColName)) {
           throw new MalformedDataMapCommandException(
@@ -119,7 +121,7 @@ case class PreAggregateTableHelper(
               + " does not in datamap")
         }
         newColName
-      })
+      }
       tableProperties.put(CarbonCommonConstants.LONG_STRING_COLUMNS,
         newLongStringColumn.mkString(","))
     }
