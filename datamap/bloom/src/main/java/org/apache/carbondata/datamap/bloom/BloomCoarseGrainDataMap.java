f8711d80b06497161635b6327a80f2d9ebf2e5c9
xuchuanyin <xuchuanyin@hust.edu.cn>
2018-07-30 17:50:51 +0800

    Fix bugs in querying with bloom datamap on preagg with dictionary column
    For preaggregate table, if the groupby column is dictionary column in
    parent table, the preaggregate table will inherit the dictionary
    encoding as well as the dictionary file from the parent table.
    
    So for dictionary columns, during query with bloom, we need to
    convert the plain filter value to dictionarty encoded value based on
    parent table's dictionary file.

--- datamap/bloom/src/main/java/org/apache/carbondata/datamap/bloom/BloomCoarseGrainDataMap.java
+++ datamap/bloom/src/main/java/org/apache/carbondata/datamap/bloom/BloomCoarseGrainDataMap.java
@@ -47,10 +47,12 @@ import org.apache.carbondata.core.devapi.DictionaryGenerationException;
 import org.apache.carbondata.core.indexstore.Blocklet;
 import org.apache.carbondata.core.indexstore.PartitionSpec;
 import org.apache.carbondata.core.metadata.AbsoluteTableIdentifier;
+import org.apache.carbondata.core.metadata.CarbonMetadata;
 import org.apache.carbondata.core.metadata.datatype.DataType;
 import org.apache.carbondata.core.metadata.datatype.DataTypes;
 import org.apache.carbondata.core.metadata.encoder.Encoding;
 import org.apache.carbondata.core.metadata.schema.table.CarbonTable;
+import org.apache.carbondata.core.metadata.schema.table.RelationIdentifier;
 import org.apache.carbondata.core.metadata.schema.table.column.CarbonColumn;
 import org.apache.carbondata.core.scan.expression.ColumnExpression;
 import org.apache.carbondata.core.scan.expression.Expression;
@@ -108,6 +110,14 @@ public class BloomCoarseGrainDataMap extends CoarseGrainDataMap {
     for (CarbonColumn col : indexedColumn) {
       this.name2Col.put(col.getColName(), col);
     }
+    String parentTablePath = "";
+    if (carbonTable.isChildDataMap()) {
+      RelationIdentifier parentIdentifier =
+          carbonTable.getTableInfo().getParentRelationIdentifiers().get(0);
+      CarbonTable parentTable = CarbonMetadata.getInstance().getCarbonTable(
+          parentIdentifier.getDatabaseName(), parentIdentifier.getTableName());
+      parentTablePath = parentTable.getTablePath();
+    }
 
     try {
       this.name2Converters = new HashMap<>(indexedColumn.size());
@@ -129,7 +139,7 @@ public class BloomCoarseGrainDataMap extends CoarseGrainDataMap {
         dataField.setTimestampFormat(tsFormat);
         FieldConverter fieldConverter = FieldEncoderFactory.getInstance()
             .createFieldEncoder(dataField, absoluteTableIdentifier, i, nullFormat, null, false,
-                localCaches[i], false, carbonTable.getTablePath());
+                localCaches[i], false, parentTablePath);
         this.name2Converters.put(indexedColumn.get(i).getColName(), fieldConverter);
       }
     } catch (IOException e) {
