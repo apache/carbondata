9709ff22ac38c1322daacd1e6c51d63a3c637a8b
Indhumathi27 <indhumathim27@gmail.com>
2018-08-08 15:33:36 +0530

    [CARBONDATA-2840] Added SDV testcases for Complex DataType Support

--- integration/spark-common-cluster-test/src/test/scala/org/apache/carbondata/cluster/sdv/suite/SDVSuites.scala
+++ integration/spark-common-cluster-test/src/test/scala/org/apache/carbondata/cluster/sdv/suite/SDVSuites.scala
@@ -153,6 +153,7 @@ class SDVSuites3 extends Suites with BeforeAndAfterAll {
                     new TestPartitionWithGlobalSort ::
                     new SDKwriterTestCase ::
                     new SetParameterTestCase ::
+                    new ComplexDataTypeTestCase ::
                     new PartitionWithPreAggregateTestCase ::
                     new CreateTableWithLocalDictionaryTestCase :: Nil
 
