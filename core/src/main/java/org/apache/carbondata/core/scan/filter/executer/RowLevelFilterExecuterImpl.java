ec29dd1069db18f33e77f46fb763b6288790ba55
kumarvishal09 <kumarvishal1802@gmail.com>
2018-10-16 13:04:26 +0530

    PrimitiveDimColumnPage

--- core/src/main/java/org/apache/carbondata/core/scan/filter/executer/RowLevelFilterExecuterImpl.java
+++ core/src/main/java/org/apache/carbondata/core/scan/filter/executer/RowLevelFilterExecuterImpl.java
@@ -35,6 +35,7 @@ import org.apache.carbondata.core.cache.dictionary.Dictionary;
 import org.apache.carbondata.core.constants.CarbonCommonConstants;
 import org.apache.carbondata.core.datastore.block.SegmentProperties;
 import org.apache.carbondata.core.datastore.chunk.DimensionColumnPage;
+import org.apache.carbondata.core.datastore.chunk.impl.AbstractPrimitiveDimColumnPage;
 import org.apache.carbondata.core.datastore.chunk.impl.VariableLengthDimensionColumnPage;
 import org.apache.carbondata.core.datastore.chunk.store.ColumnPageWrapper;
 import org.apache.carbondata.core.datastore.page.ColumnPage;
@@ -409,7 +410,8 @@ public class RowLevelFilterExecuterImpl implements FilterExecuter {
                 .decodeColumnPage(pageIndex);
         if (!dimColumnEvaluatorInfo.getDimension().hasEncoding(Encoding.DICTIONARY) && (
             columnDataChunk instanceof VariableLengthDimensionColumnPage
-                || columnDataChunk instanceof ColumnPageWrapper)) {
+                || columnDataChunk instanceof ColumnPageWrapper
+                || columnDataChunk instanceof AbstractPrimitiveDimColumnPage)) {
 
           byte[] memberBytes = columnDataChunk.getChunkData(index);
           if (null != memberBytes) {
