25163c09393332e4f53eb88b7059b8d3f09a4388
dhatchayani <dhatcha.official@gmail.com>
2018-11-13 18:28:48 +0530

    [CARBONDATA-3096] Wrong records size on the input metrics & Free the intermediate page used while adaptive encoding

--- integration/spark-datasource/src/main/scala/org/apache/carbondata/spark/vectorreader/VectorizedCarbonRecordReader.java
+++ integration/spark-datasource/src/main/scala/org/apache/carbondata/spark/vectorreader/VectorizedCarbonRecordReader.java
@@ -161,8 +161,8 @@ public class VectorizedCarbonRecordReader extends AbstractRecordReader<Object> {
 
   @Override
   public void close() throws IOException {
-    logStatistics(rowCount, queryModel.getStatisticsRecorder());
     if (vectorProxy != null) {
+      logStatistics(rowCount, queryModel.getStatisticsRecorder());
       vectorProxy.close();
       vectorProxy = null;
     }
@@ -198,7 +198,7 @@ public class VectorizedCarbonRecordReader extends AbstractRecordReader<Object> {
   @Override
   public Object getCurrentValue() throws IOException, InterruptedException {
     if (returnColumnarBatch) {
-      int value = vectorProxy.numRows();
+      int value = carbonColumnarBatch.getActualSize();
       rowCount += value;
       if (inputMetricsStats != null) {
         inputMetricsStats.incrementRecordRead((long) value);
