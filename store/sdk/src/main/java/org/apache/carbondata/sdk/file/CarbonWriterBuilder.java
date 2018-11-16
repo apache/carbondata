d12c0c9bdc77610edca4ff07bb2224088a5f0de1
xuchuanyin <xuchuanyin@hust.edu.cn>
2018-11-16 14:54:22 +0800

    Update store/sdk/src/main/java/org/apache/carbondata/sdk/file/CarbonWriterBuilder.java
    Co-Authored-By: jackylk <jacky.likun@huawei.com>

--- store/sdk/src/main/java/org/apache/carbondata/sdk/file/CarbonWriterBuilder.java
+++ store/sdk/src/main/java/org/apache/carbondata/sdk/file/CarbonWriterBuilder.java
@@ -443,7 +443,7 @@ public class CarbonWriterBuilder {
     }
     if (this.writtenByApp == null || this.writtenByApp.isEmpty()) {
       throw new RuntimeException(
-          "'writtenBy' must be set when writting carbon files, use writtenBy() API to "
+          "'writtenBy' must be set when writing carbon files, use writtenBy() API to "
               + "set it, it can be the name of the application which is using the SDK");
     }
     CarbonLoadModel loadModel = buildLoadModel(schema);
