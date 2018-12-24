0c777259d54d3b99acd9d62b75cddde7e726afc1
ChandraSaripaka <chandras4bigdata@gmail.com>
2018-12-24 15:19:30 +0800

    CARBONDATA-3193: Build support for CDH 5.14.2 Spark 2.2.0

---
+++ integration/spark-datasource/src/main/spark-2.2.0cloudera-5.14.2/org/apache/spark/sql/CarbonDictionaryWrapper.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.sql;
+
+import org.apache.carbondata.core.scan.result.vector.CarbonDictionary;
+
+import parquet.column.Dictionary;
+import parquet.column.Encoding;
+import parquet.io.api.Binary;
+
+public class CarbonDictionaryWrapper extends Dictionary {
+
+  /**
+   * dictionary values
+   */
+  private Binary[] binaries;
+
+  CarbonDictionaryWrapper(Encoding encoding , CarbonDictionary dictionary) {
+    super(encoding);
+    binaries = new Binary[dictionary.getDictionarySize()];
+    for (int i = 0; i < binaries.length; i++) {
+      binaries[i] = Binary.fromByteArray(dictionary.getDictionaryValue(i));
+    }
+  }
+
+  @Override public int getMaxId() {
+    return binaries.length - 1;
+  }
+
+  @Override public Binary decodeToBinary(int id) {
+    return binaries[id];
+  }
+}
