02484bd6ed2574a93d2d8632d1ac7959455a1852
binw305@163.com <513338github>
2019-01-07 23:34:55 +0800

    【CARBONDATA-3211】Optimize the documentation

--- datamap/lucene/src/main/java/org/apache/carbondata/datamap/lucene/LuceneFineGrainDataMapFactory.java
+++ datamap/lucene/src/main/java/org/apache/carbondata/datamap/lucene/LuceneFineGrainDataMapFactory.java
@@ -57,7 +57,7 @@ public class LuceneFineGrainDataMapFactory extends LuceneDataMapFactoryBase<Fine
           DataMapWriter.getDefaultDataMapPath(tableIdentifier.getTablePath(),
               segment.getSegmentNo(), dataMapName), segment.getConfiguration()));
     } catch (MemoryException e) {
-      LOGGER.error("failed to get lucene datamap, detail is {}" + e.getMessage());
+      LOGGER.error(String.format("failed to get lucene datamap, detail is %s", e.getMessage()));
       return lstDataMap;
     }
     lstDataMap.add(dataMap);
