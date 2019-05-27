fa3e392c17ff1867baa6ac1ae918346e76ac1add
kunal642 <kunalkapoor642@gmail.com>
2019-05-27 12:41:54 +0530

    [CARBONDATA-3399] Implement executor id based distribution for indexserver
    This closes #3237

--- integration/spark2/src/main/scala/org/apache/carbondata/indexserver/DistributedRDDUtils.scala
+++ integration/spark2/src/main/scala/org/apache/carbondata/indexserver/DistributedRDDUtils.scala
@@ -20,7 +20,6 @@ import java.util.concurrent.ConcurrentHashMap
 
 import scala.collection.JavaConverters._
 
-import org.apache.commons.lang.StringUtils
 import org.apache.hadoop.mapreduce.InputSplit
 import org.apache.spark.Partition
 
@@ -29,14 +28,14 @@ import org.apache.carbondata.core.datamap.dev.expr.DataMapDistributableWrapper
 
 object DistributedRDDUtils {
   // Segment number to executorNode mapping
-  val segmentToExecutorMapping: java.util.Map[String, String] =
-    new ConcurrentHashMap[String, String]()
+  val tableToExecutorMapping: ConcurrentHashMap[String, ConcurrentHashMap[String, String]] =
+    new ConcurrentHashMap[String, ConcurrentHashMap[String, String]]()
 
   // executorNode to segmentSize mapping
-  val executorToCacheSizeMapping: java.util.Map[String, Long] =
-    new ConcurrentHashMap[String, Long]()
+  val executorToCacheSizeMapping: ConcurrentHashMap[String, ConcurrentHashMap[String, Long]] =
+    new ConcurrentHashMap[String, ConcurrentHashMap[String, Long]]()
 
-  def getExecutors(segment: Array[InputSplit], executorsList : Set[String],
+  def getExecutors(segment: Array[InputSplit], executorsList : Map[String, Seq[String]],
       tableUniqueName: String, rddId: Int): Seq[Partition] = {
     // sort the partitions in increasing order of index size.
     val (segments, legacySegments) = segment.span(split => split
@@ -47,10 +46,23 @@ object DistributedRDDUtils {
                                               .getDistributable.getSegment.getIndexSize)
     val executorCache = DistributedRDDUtils.executorToCacheSizeMapping
     // check if any executor is dead.
-    val invalidExecutors = executorCache.keySet().asScala.diff(executorsList)
-    if (invalidExecutors.nonEmpty) {
+    val invalidHosts = executorCache.keySet().asScala.diff(executorsList.keySet)
+    if (invalidHosts.nonEmpty) {
       // extract the dead executor host name
-      DistributedRDDUtils.invalidateExecutors(invalidExecutors.toSeq)
+      DistributedRDDUtils.invalidateHosts(invalidHosts.toSeq)
+    }
+    val invalidExecutorIds = executorsList.collect {
+      case (host, executors) if executorCache.get(host) != null =>
+        val toBeRemovedExecutors = executorCache.get(host).keySet().asScala.diff(executors.toSet)
+        if (executors.size == toBeRemovedExecutors.size) {
+          DistributedRDDUtils.invalidateHosts(Seq(host))
+          Seq()
+        } else {
+          toBeRemovedExecutors.map(executor => host + "_" + executor)
+        }
+    }.flatten
+    if (invalidExecutorIds.nonEmpty) {
+      DistributedRDDUtils.invalidateExecutors(invalidExecutorIds.toSeq)
     }
     (convertToPartition(legacySegments, tableUniqueName, executorsList) ++
      convertToPartition(sortedPartitions, tableUniqueName, executorsList)).zipWithIndex.map {
@@ -60,7 +72,7 @@ object DistributedRDDUtils {
   }
 
   private def convertToPartition(segments: Seq[InputSplit], tableUniqueName: String,
-      executorList: Set[String]): Seq[InputSplit] = {
+      executorList: Map[String, Seq[String]]): Seq[InputSplit] = {
     segments.map { partition =>
       val wrapper: DataMapDistributable = partition.asInstanceOf[DataMapDistributableWrapper]
         .getDistributable
@@ -82,19 +94,27 @@ object DistributedRDDUtils {
         executorCacheSize =>
           // executorCacheSize would be in the form of 127.0.0.1_10024 where the left of '_'
           // would be the executor IP and the right would be the cache that executor is holding.
-          val executorIP = executorCacheSize.substring(0, executorCacheSize.lastIndexOf('_'))
+          val hostAndExecutor = executorCacheSize.substring(0,
+            executorCacheSize.lastIndexOf('_'))
+          val (host, executor) = (hostAndExecutor
+            .substring(0, hostAndExecutor.lastIndexOf('_')), hostAndExecutor
+            .substring(hostAndExecutor.lastIndexOf('_') + 1, hostAndExecutor.length))
           val size = executorCacheSize.substring(executorCacheSize.lastIndexOf('_') + 1,
             executorCacheSize.length)
-          executorToCacheSizeMapping.put(executorIP, size.toLong)
+          val executorMapping = executorToCacheSizeMapping.get(host)
+          if (executorMapping != null) {
+            executorMapping.put(executor, size.toLong)
+            executorToCacheSizeMapping.put(host, executorMapping)
+          }
       }
     }
   }
 
   def invalidateCache(tableUniqueName: String): Unit = {
-    segmentToExecutorMapping.keySet().asScala.foreach {
+    tableToExecutorMapping.keySet().asScala.foreach {
       key =>
-        if (key.split("_")(0).equalsIgnoreCase(tableUniqueName)) {
-          segmentToExecutorMapping.remove(key)
+        if (key.equalsIgnoreCase(tableUniqueName)) {
+          tableToExecutorMapping.remove(key)
         }
     }
   }
@@ -103,22 +123,77 @@ object DistributedRDDUtils {
    * Invalidate the dead executors from the mapping and assign the segments to some other
    * executor, so that the query can load the segments to the new assigned executor.
    */
-  def invalidateExecutors(invalidExecutors: Seq[String]): Unit = synchronized {
+  def invalidateHosts(invalidHosts: Seq[String]): Unit = {
+    synchronized {
+      val validInvalidExecutors: Map[String, String] = invalidHosts.flatMap {
+        host =>
+          val invalidExecutorToSizeMapping = executorToCacheSizeMapping.remove(host)
+          invalidExecutorToSizeMapping.asScala.map {
+            case (invalidExecutor, size) =>
+              getLeastLoadedExecutor match {
+                case Some((reassignedHost, reassignedExecutorId)) =>
+                  val existingExecutorMapping = executorToCacheSizeMapping.get(reassignedHost)
+                  if (existingExecutorMapping != null) {
+                    val existingSize = existingExecutorMapping.get(reassignedExecutorId)
+                    existingExecutorMapping.put(reassignedExecutorId, existingSize + size)
+                  } else {
+                    existingExecutorMapping.put(reassignedExecutorId, size)
+                  }
+                  executorToCacheSizeMapping.put(reassignedHost, existingExecutorMapping)
+                  s"${host}_$invalidExecutor" -> s"${ reassignedHost }_$reassignedExecutorId"
+                case None => "" -> ""
+              }
+          }
+      }.toMap
+      updateTableMappingForInvalidExecutors(validInvalidExecutors)
+    }
+  }
+
+  private def updateTableMappingForInvalidExecutors(validInvalidExecutors: Map[String, String]) {
     // remove all invalidExecutor mapping from cache.
-    for ((key: String, value: String) <- segmentToExecutorMapping.asScala) {
+    for ((tableName: String, segmentToExecutorMapping) <- tableToExecutorMapping.asScala) {
       // find the invalid executor in cache.
-      if (invalidExecutors.contains(value)) {
-        // remove mapping for the invalid executor.
-        val invalidExecutorSize = executorToCacheSizeMapping.remove(key)
-        // find a new executor for the segment
-        val reassignedExecutor = getLeastLoadedExecutor
-        segmentToExecutorMapping.put(key, reassignedExecutor)
-        // add the size size of the invalid executor to the reassigned executor.
-        executorToCacheSizeMapping.put(
-          reassignedExecutor,
-          executorToCacheSizeMapping.get(reassignedExecutor) + invalidExecutorSize
-        )
+      val newSegmentToExecutorMapping = new ConcurrentHashMap[String, String]()
+      val existingMapping = tableToExecutorMapping.get(tableName)
+      segmentToExecutorMapping.asScala.collect {
+        case (segmentNumber, executorUniqueName) if validInvalidExecutors
+          .contains(executorUniqueName) =>
+          val newExecutorId = validInvalidExecutors(executorUniqueName)
+          // remove mapping for the invalid executor.
+          val executorIdSplits = newExecutorId.split("_")
+          val (host, executorId) = (executorIdSplits(0), executorIdSplits(1))
+          // find a new executor for the segment
+          newSegmentToExecutorMapping
+            .put(segmentNumber, s"${ host }_$executorId")
       }
+      existingMapping.putAll(newSegmentToExecutorMapping)
+      tableToExecutorMapping.put(tableName, existingMapping)
+    }
+  }
+
+  def invalidateExecutors(invalidExecutors: Seq[String]): Unit = {
+    synchronized {
+      val validInvalidExecutors: Map[String, String] = invalidExecutors.map {
+        invalidExecutor =>
+          val executorIdSplits = invalidExecutor.split("_")
+          val (host, executor) = (executorIdSplits(0), executorIdSplits(1))
+          val invalidExecutorSize = executorToCacheSizeMapping.get(host).remove(executor)
+          getLeastLoadedExecutor match {
+            case Some((reassignedHost, reassignedExecutorId)) =>
+              val existingExecutorMapping = executorToCacheSizeMapping.get(reassignedHost)
+              if (existingExecutorMapping != null) {
+                val existingSize = existingExecutorMapping.get(reassignedExecutorId)
+                existingExecutorMapping
+                  .put(reassignedExecutorId, existingSize + invalidExecutorSize)
+              } else {
+                existingExecutorMapping.put(reassignedExecutorId, invalidExecutorSize)
+              }
+              executorToCacheSizeMapping.put(reassignedHost, existingExecutorMapping)
+              invalidExecutor -> s"${ reassignedHost }_$reassignedExecutorId"
+            case None => "" -> ""
+          }
+      }.toMap
+      updateTableMappingForInvalidExecutors(validInvalidExecutors)
     }
   }
 
@@ -127,8 +202,30 @@ object DistributedRDDUtils {
    *
    * @return
    */
-  private def getLeastLoadedExecutor: String = {
-    executorToCacheSizeMapping.asScala.toSeq.sortWith(_._2 < _._2).head._1
+  private def getLeastLoadedExecutor: Option[(String, String)] = {
+    val leastHostExecutor = executorToCacheSizeMapping.asScala.flatMap {
+      case (host, executorToCacheMap) =>
+        executorToCacheMap.asScala.map {
+          case (executor, size) =>
+            (host, executor, size)
+        }
+    }.toSeq.sortWith(_._3 < _._3).toList
+    leastHostExecutor match {
+      case head :: _ =>
+        Some(head._1, head._2)
+      case _ => None
+    }
+  }
+
+  private def checkForUnassignedExecutors(validExecutorIds: Seq[String]): Option[String] = {
+    val usedExecutorIds = executorToCacheSizeMapping.asScala.flatMap {
+      case (host, executorMap) =>
+        executorMap.keySet().asScala.map {
+          executor => s"${ host }_$executor"
+        }
+    }
+    val unassignedExecutor = validExecutorIds.diff(usedExecutorIds.toSeq)
+    unassignedExecutor.headOption
   }
 
   /**
@@ -137,27 +234,56 @@ object DistributedRDDUtils {
    *
    * @return
    */
-  def assignExecutor(tableName: String, segment: Segment, validExecutors: Set[String]): String = {
-    val cacheKey = s"${ tableName }_${ segment.getSegmentNo }"
-    val executor = segmentToExecutorMapping.get(cacheKey)
-    if (executor != null) {
-      executor
+  def assignExecutor(tableUniqueName: String,
+      segment: Segment,
+      validExecutors: Map[String, Seq[String]]): String = {
+    val segmentMapping = tableToExecutorMapping.get(tableUniqueName)
+    lazy val executor = segmentMapping.get(segment.getSegmentNo)
+    if (segmentMapping != null && executor != null) {
+      s"executor_$executor"
     } else {
       // check if any executor is not assigned. If yes then give priority to that executor
       // otherwise get the executor which has handled the least size.
-      val unassignedExecutors = validExecutors
-        .diff(executorToCacheSizeMapping.asScala.keys.toSet)
-      val newExecutor = if (unassignedExecutors.nonEmpty) {
-        unassignedExecutors.head.split(":")(0)
+      val validExecutorIds = validExecutors.flatMap {
+        case (host, executors) => executors.map {
+          executor => s"${host}_$executor"
+        }
+      }.toSeq
+      val unassignedExecutor = checkForUnassignedExecutors(validExecutorIds)
+      val (newHost, newExecutor) = if (unassignedExecutor.nonEmpty) {
+        val freeExecutor = unassignedExecutor.get.split("_")
+        (freeExecutor(0), freeExecutor(1))
       } else {
-        val identifiedExecutor = getLeastLoadedExecutor
-        identifiedExecutor
+        getLeastLoadedExecutor match {
+          case Some((host, executorID)) => (host, executorID)
+          case None => throw new RuntimeException("Could not find any alive executors.")
+        }
       }
-      val existingExecutorSize = executorToCacheSizeMapping.get(newExecutor)
-      executorToCacheSizeMapping.put(newExecutor, existingExecutorSize + segment.getIndexSize
-        .toInt)
-      segmentToExecutorMapping.put(cacheKey, newExecutor)
-      newExecutor
+      val existingExecutorMapping = executorToCacheSizeMapping.get(newHost)
+      if (existingExecutorMapping != null) {
+        val existingSize = existingExecutorMapping.get(newExecutor)
+        if (existingSize != null) {
+          existingExecutorMapping.put(newExecutor, existingSize + segment.getIndexSize
+            .toInt)
+        } else {
+          existingExecutorMapping.put(newExecutor, segment.getIndexSize
+            .toInt)
+        }
+      } else {
+        val newExecutorMapping = new ConcurrentHashMap[String, Long]()
+        newExecutorMapping.put(newExecutor, segment.getIndexSize)
+        executorToCacheSizeMapping.put(newHost, newExecutorMapping)
+      }
+      val existingSegmentMapping = tableToExecutorMapping.get(tableUniqueName)
+      if (existingSegmentMapping == null) {
+        val newSegmentMapping = new ConcurrentHashMap[String, String]()
+        newSegmentMapping.put(segment.getSegmentNo, s"${newHost}_$newExecutor")
+        tableToExecutorMapping.put(tableUniqueName, newSegmentMapping)
+      } else {
+        existingSegmentMapping.put(segment.getSegmentNo, s"${newHost}_$newExecutor")
+        tableToExecutorMapping.put(tableUniqueName, existingSegmentMapping)
+      }
+      s"executor_${newHost}_$newExecutor"
     }
   }
 
