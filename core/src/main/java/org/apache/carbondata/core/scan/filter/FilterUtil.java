e0188a55dbf106271e2ede5b4db7973d4f1d963a
manhua <kevinjmh@qq.com>
2019-06-12 16:57:59 +0800

    pr3209

--- core/src/main/java/org/apache/carbondata/core/scan/filter/FilterUtil.java
+++ core/src/main/java/org/apache/carbondata/core/scan/filter/FilterUtil.java
@@ -1535,6 +1535,7 @@ public final class FilterUtil {
         }
       }
       msrColumnExecuterInfo.setFilterKeys(keysBasedOnFilter);
+      msrColumnExecuterInfo.setFilterKeysSet(keysBasedOnFilter);
     } else {
       if (filterValues == null) {
         dimColumnExecuterInfo.setFilterKeys(new byte[0][]);
