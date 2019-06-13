0c228d7339e72be1436ec0e210d4e6d2e2bd8675
manishnalla1994 <manish.nalla1994@gmail.com>
2019-06-13 10:11:41 +0530

    Added enable/disable Range column compaction

--- core/src/main/java/org/apache/carbondata/core/constants/CarbonCommonConstants.java
+++ core/src/main/java/org/apache/carbondata/core/constants/CarbonCommonConstants.java
@@ -1205,6 +1205,10 @@ public final class CarbonCommonConstants {
 
   public static final String CARBON_RANGE_COLUMN_SCALE_FACTOR_DEFAULT = "3";
 
+  public static final String CARBON_ENABLE_RANGE_COMPACTION = "carbon.enable.range.compaction";
+
+  public static final String CARBON_ENABLE_RANGE_COMPACTION_DEFAULT = "true";
+
   //////////////////////////////////////////////////////////////////////////////////////////
   // Query parameter start here
   //////////////////////////////////////////////////////////////////////////////////////////
