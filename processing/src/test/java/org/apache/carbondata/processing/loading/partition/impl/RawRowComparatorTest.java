1030eea09efb5f6ac6d0ed6e5f59437214205f93
Vikram Ahuja <vikramahuja8803@gmail.com>
2019-08-12 14:03:34 +0530

    headers added

--- processing/src/test/java/org/apache/carbondata/processing/loading/partition/impl/RawRowComparatorTest.java
+++ processing/src/test/java/org/apache/carbondata/processing/loading/partition/impl/RawRowComparatorTest.java
@@ -15,12 +15,13 @@
  * limitations under the License.
  */
 
-package org.apache.carbondata.processing.sort.sortdata;
+package org.apache.carbondata.processing.loading.partition.impl;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.carbondata.core.datastore.row.CarbonRow;
 import org.apache.carbondata.core.metadata.datatype.DataType;
 import org.apache.carbondata.core.metadata.datatype.DataTypes;
 import org.apache.carbondata.core.util.comparator.SerializableComparator;
@@ -28,14 +29,15 @@ import org.apache.carbondata.core.util.comparator.SerializableComparator;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class NewRowComparatorTest {
-  private NewRowComparator new_comparator;
+public class RawRowComparatorTest {
+  private RawRowComparator new_comparator;
 
   @Test public void checkmap() {
     DataType noDicDataTypes[] =
         { DataTypes.INT, DataTypes.SHORT, DataTypes.BOOLEAN, DataTypes.BYTE, DataTypes.LONG };
-    boolean noDicSortColumnMapping[] = { true, true, true, true, true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
+    boolean isSortColumnNoDict[] = { true, true, true, true, true };
+    int sortColumnIndices[] = { 1, 2, 3, 4, 5 };
+    new_comparator = new RawRowComparator(sortColumnIndices, isSortColumnNoDict, noDicDataTypes);
     Field privateField = null;
     try {
       privateField = new_comparator.getClass().getDeclaredField("comparator_map");
@@ -52,84 +54,103 @@ public class NewRowComparatorTest {
       throw new RuntimeException(e);
     }
   }
-
+ 
   @Test public void compareint() {
     DataType noDicDataTypes[] = { DataTypes.INT };
     boolean noDicSortColumnMapping[] = { true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
+    int sortColumnIndices[] = { 1 };
+    new_comparator =
+        new RawRowComparator(sortColumnIndices, noDicSortColumnMapping, noDicDataTypes);
+
     Integer arr[] = { 1, 7, 5 };
     Integer arr1[] = { 2, 4, 6 };
-    int res = new_comparator.compare(arr, arr1);
-    Assert.assertTrue(res < 0);
+    int res = new_comparator.compare(new CarbonRow(arr), new CarbonRow(arr1));
+    Assert.assertTrue(res > 0);
   }
 
   @Test public void compareintreverse() {
     DataType noDicDataTypes[] = { DataTypes.INT };
     boolean noDicSortColumnMapping[] = { true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
-    Integer arr[] = { 1, 2, 3 };
-    Integer arr1[] = { 4, 5, 6 };
-    int res = new_comparator.compare(arr1, arr);
-    Assert.assertTrue(res > 0);
+    int sortColumnIndices[] = { 1 };
+    new_comparator =
+        new RawRowComparator(sortColumnIndices, noDicSortColumnMapping, noDicDataTypes);
+    
+    Integer arr[] = { 2, 7, 5 };
+    Integer arr1[] = { 1, 4, 6 };
+    int res = new_comparator.compare(new CarbonRow(arr1), new CarbonRow(arr));
+    Assert.assertTrue(res < 0);
   }
 
   @Test public void compareshort() {
     DataType noDicDataTypes[] = { DataTypes.SHORT };
     boolean noDicSortColumnMapping[] = { true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
-    Short arr[] = { 1, 2, 3 };
-    Short arr1[] = { 4, 5, 6 };
-    int res = new_comparator.compare(arr, arr1);
-    Assert.assertTrue(res < 0);
+    int sortColumnIndices[] = { 1 };
+    new_comparator =
+        new RawRowComparator(sortColumnIndices, noDicSortColumnMapping, noDicDataTypes);
+
+    Short arr[] = { 1, 7, 5 };
+    Short arr1[] = { 2, 4, 6 };
+    int res = new_comparator.compare(new CarbonRow(arr), new CarbonRow(arr1));
+    Assert.assertTrue(res > 0);
   }
 
   @Test public void comparelong() {
     DataType noDicDataTypes[] = { DataTypes.LONG };
     boolean noDicSortColumnMapping[] = { true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
-    Long arr[] = { 1L, 2L, 3L };
-    Long arr1[] = { 4L, 5L, 6L };
-    int res = new_comparator.compare(arr, arr1);
-    Assert.assertTrue(res < 0);
+    int sortColumnIndices[] = { 1 };
+    new_comparator =
+        new RawRowComparator(sortColumnIndices, noDicSortColumnMapping, noDicDataTypes);
+    Long arr[] = { 1L, 7L, 5L };
+    Long arr1[] = { 2L, 4L, 6L };
+    int res = new_comparator.compare(new CarbonRow(arr), new CarbonRow(arr1));
+    Assert.assertTrue(res > 0);
   }
 
   @Test public void comparefloat() {
     DataType noDicDataTypes[] = { DataTypes.FLOAT };
     boolean noDicSortColumnMapping[] = { true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
-    Float arr[] = { 1F, 2F, 3F };
-    Float arr1[] = { 4F, 5F, 6F };
-    int res = new_comparator.compare(arr, arr1);
-    Assert.assertTrue(res < 0);
+    int sortColumnIndices[] = { 1 };
+    new_comparator =
+        new RawRowComparator(sortColumnIndices, noDicSortColumnMapping, noDicDataTypes);
+    Float arr[] = { 1F, 7F, 5F };
+    Float arr1[] = { 2F, 4F, 6F };
+    int res = new_comparator.compare(new CarbonRow(arr), new CarbonRow(arr1));
+    Assert.assertTrue(res > 0);
   }
 
   @Test public void compareboolean() {
     DataType noDicDataTypes[] = { DataTypes.BOOLEAN };
     boolean noDicSortColumnMapping[] = { true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
-    Boolean arr[] = { false };
-    Boolean arr1[] = { true };
-    int res = new_comparator.compare(arr, arr1);
+    int sortColumnIndices[] = { 1 };
+    new_comparator =
+        new RawRowComparator(sortColumnIndices, noDicSortColumnMapping, noDicDataTypes);
+    Boolean arr[] = { false, false };
+    Boolean arr1[] = { true, true };
+    int res = new_comparator.compare(new CarbonRow(arr), new CarbonRow(arr1));
     Assert.assertTrue(res < 0);
   }
 
-  @Test public void comparebyte() {
+   @Test public void comparebyte() {
     DataType noDicDataTypes[] = { DataTypes.BYTE };
     boolean noDicSortColumnMapping[] = { true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
+    int sortColumnIndices[] = { 1 };
+    new_comparator =
+        new RawRowComparator(sortColumnIndices, noDicSortColumnMapping, noDicDataTypes);
     Byte arr[] = { 1, 2, 3 };
     Byte arr1[] = { 4, 5, 6 };
-    int res = new_comparator.compare(arr, arr1);
+    int res = new_comparator.compare(new CarbonRow(arr), new CarbonRow(arr1));
     Assert.assertTrue(res < 0);
   }
- 
+
   @Test public void comparemixed() {
-    DataType noDicDataTypes[] = { DataTypes.INT, DataTypes.FLOAT, DataTypes.BOOLEAN };
+    DataType noDicDataTypes[] = { DataTypes.INT, DataTypes.SHORT, DataTypes.BOOLEAN };
     boolean noDicSortColumnMapping[] = { true, true, true };
-    new_comparator = new NewRowComparator(noDicSortColumnMapping, noDicDataTypes);
-    Object arr[] = { 1, 2F, false };
-    Object arr1[] = { 4, 5F, true };
-    int res = new_comparator.compare(arr, arr1);
+    int sortColumnIndices[] = { 1, 2, 3 };
+    new_comparator =
+        new RawRowComparator(sortColumnIndices, noDicSortColumnMapping, noDicDataTypes);
+    Object arr[] = { 1, 2, false };
+    Object arr1[] = { 4, 5, true };
+    int res = new_comparator.compare(new CarbonRow(arr), new CarbonRow(arr1));
     Assert.assertTrue(res < 0);
   }
 }
