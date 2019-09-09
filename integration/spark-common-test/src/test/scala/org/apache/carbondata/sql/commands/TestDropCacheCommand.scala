123c32da8f62b3f5e8bc8bd623959e80f440a949
Ajith <ajith2489@gmail.com>
2019-09-09 08:15:14 +0530

    Split CarbonBased and SparkBased tests

--- integration/spark-common-test/src/test/scala/org/apache/carbondata/sql/commands/TestDropCacheCommand.scala
+++ integration/spark-common-test/src/test/scala/org/apache/carbondata/sql/commands/TestDropCacheCommand.scala
@@ -23,12 +23,12 @@ import scala.collection.JavaConverters._
 
 import org.apache.spark.sql.CarbonEnv
 import org.apache.spark.sql.catalyst.TableIdentifier
-import org.apache.spark.sql.test.util.CarbonQueryTest
+import org.apache.spark.sql.test.util.QueryTest
 import org.scalatest.BeforeAndAfterAll
 import org.apache.carbondata.core.cache.CacheProvider
 import org.apache.carbondata.core.constants.CarbonCommonConstants
 
-class TestCarbonDropCacheCommand extends CarbonQueryTest with BeforeAndAfterAll {
+class TestDropCacheCommand extends QueryTest with BeforeAndAfterAll {
 
   val dbName = "cache_db"
 
@@ -42,13 +42,15 @@ class TestCarbonDropCacheCommand extends CarbonQueryTest with BeforeAndAfterAll 
     sql(s"use default")
     sql(s"DROP DATABASE $dbName CASCADE")
   }
+
   test("Test dictionary") {
     val tableName = "t1"
 
     sql(s"CREATE TABLE $tableName(empno int, empname String, designation String, " +
         s"doj Timestamp, workgroupcategory int, workgroupcategoryname String, deptno int, " +
         s"deptname String, projectcode int, projectjoindate Timestamp, projectenddate Timestamp," +
-        s"attendance int,utilization int, salary int) stored by 'carbondata' ")
+        s"attendance int,utilization int, salary int) stored by 'carbondata' " +
+        s"TBLPROPERTIES('DICTIONARY_INCLUDE'='designation, workgroupcategoryname')")
     sql(s"LOAD DATA INPATH '$resourcesPath/data.csv' INTO TABLE $tableName")
     sql(s"LOAD DATA INPATH '$resourcesPath/data.csv' INTO TABLE $tableName")
     sql(s"SELECT * FROM $tableName").collect()
@@ -81,45 +83,6 @@ class TestCarbonDropCacheCommand extends CarbonQueryTest with BeforeAndAfterAll 
     for (dictId <- dictIds) {
       assert(!cacheAfterDrop.asScala.exists(key => key.contains(dictId)))
     }
-  }
-
-  test("Test bloom filter") {
-    val tableName = "t3"
-
-    sql(s"CREATE TABLE $tableName(empno int, empname String, designation String, " +
-        s"doj Timestamp, workgroupcategory int, workgroupcategoryname String, deptno int, " +
-        s"deptname String, projectcode int, projectjoindate Timestamp, projectenddate Timestamp," +
-        s"attendance int, utilization int, salary int) stored by 'carbondata'")
-    sql(s"CREATE DATAMAP dblom ON TABLE $tableName USING 'bloomfilter' " +
-        "DMPROPERTIES('INDEX_COLUMNS'='deptno')")
-    sql(s"LOAD DATA INPATH '$resourcesPath/data.csv' INTO TABLE $tableName")
-    sql(s"SELECT * FROM $tableName").collect()
-    sql(s"SELECT * FROM $tableName WHERE deptno=10").collect()
-
-    val droppedCacheKeys = clone(CacheProvider.getInstance().getCarbonCache.getCacheMap.keySet())
-
-    sql(s"DROP METACACHE ON TABLE $tableName")
-
-    val cacheAfterDrop = clone(CacheProvider.getInstance().getCarbonCache.getCacheMap.keySet())
-    droppedCacheKeys.removeAll(cacheAfterDrop)
-
-    val tableIdentifier = new TableIdentifier(tableName, Some(dbName))
-    val carbonTable = CarbonEnv.getCarbonTable(tableIdentifier)(sqlContext.sparkSession)
-    val tablePath = carbonTable.getTablePath
-    val bloomPath = tablePath + CarbonCommonConstants.FILE_SEPARATOR + "dblom" +
-                    CarbonCommonConstants.FILE_SEPARATOR
-
-    // Check if table index entries are dropped
-    assert(droppedCacheKeys.asScala.exists(key => key.startsWith(tablePath)))
-
-    // check if cache does not have any more table index entries
-    assert(!cacheAfterDrop.asScala.exists(key => key.startsWith(tablePath)))
-
-    // Check if bloom entries are dropped
-    assert(droppedCacheKeys.asScala.exists(key => key.contains(bloomPath)))
-
-    // check if cache does not have any more bloom entries
-    assert(!cacheAfterDrop.asScala.exists(key => key.contains(bloomPath)))
   }
 
   def clone(oldSet: util.Set[String]): util.HashSet[String] = {
