7830662cbeadb765238c59ea63821a8974477997
Ajith <ajith2489@gmail.com>
2019-10-25 14:49:13 +0530

    Dry run with partition fix

--- integration/spark-common/src/main/scala/org/apache/carbondata/spark/util/CarbonScalaUtil.scala
+++ integration/spark-common/src/main/scala/org/apache/carbondata/spark/util/CarbonScalaUtil.scala
@@ -173,9 +173,9 @@ object CarbonScalaUtil {
     try {
       column.getDataType match {
         case CarbonDataTypes.TIMESTAMP =>
-          DateTimeUtils.timestampToString(value.toLong * 1000)
+          value
         case CarbonDataTypes.DATE =>
-          DateTimeUtils.dateToString(DateTimeUtils.millisToDays(value.toLong))
+          value
         case _ => value
       }
     } catch {
