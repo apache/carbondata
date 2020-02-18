36d7d00e5bf765f0805bab08b1959e2885cd6b6e
xubo245 <601450868@qq.com>
2020-02-18 23:19:11 +0800

    remove Net and call it

--- python/pycarbon/tests/mnist/dataset_with_unischema/pytorch_example_carbon.py
+++ python/pycarbon/tests/mnist/dataset_with_unischema/pytorch_example_carbon.py
@@ -20,7 +20,6 @@ import argparse
 import jnius_config
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import transforms
@@ -31,27 +30,7 @@ from pycarbon.tests import DEFAULT_CARBONSDK_PATH
 
 from petastorm.pytorch import DataLoader
 from petastorm import TransformSpec
-
-
-class Net(nn.Module):
-  def __init__(self):
-    super(Net, self).__init__()
-    self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-    self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-    self.conv2_drop = nn.Dropout2d()
-    self.fc1 = nn.Linear(320, 50)
-    self.fc2 = nn.Linear(50, 10)
-
-  # pylint: disable=arguments-differ
-  def forward(self, x):
-    x = F.relu(F.max_pool2d(self.conv1(x), 2))
-    x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-    x = x.view(-1, 320)
-    x = F.relu(self.fc1(x))
-    x = F.dropout(x, training=self.training)
-    x = self.fc2(x)
-    return F.log_softmax(x, dim=1)
-
+from examples.mnist.pytorch_example import Net
 
 def train(model, device, train_loader, log_interval, optimizer, epoch):
   model.train()
