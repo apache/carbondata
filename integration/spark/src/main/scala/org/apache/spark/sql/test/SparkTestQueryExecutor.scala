5f12b4dbd6ceaa81a3150a267208273338ff9483
Indhumathi27 <indhumathim27@gmail.com>
2021-05-07 20:19:44 +0530

    Fix 'Cannot write incompatible data' issue

--- integration/spark/src/main/scala/org/apache/spark/sql/test/SparkTestQueryExecutor.scala
+++ integration/spark/src/main/scala/org/apache/spark/sql/test/SparkTestQueryExecutor.scala
@@ -71,6 +71,7 @@ object SparkTestQueryExecutor {
     .config("spark.sql.warehouse.dir", warehouse)
     .config("spark.sql.crossJoin.enabled", "true")
     .config("spark.sql.extensions", extensions)
+    .config("spark.sql.storeAssignmentPolicy", "legacy")
     // Default value is changed to true in hive 3.1 which dosent allow drop columns(instead use
     // replace columns when V2 is supported)
     .config("spark.hadoop.hive.metastore.disallow.incompatible.col.type.changes", "false")
