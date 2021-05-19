5959d30dc9a7e4108074928c67caf59866425b5f
Vikram Ahuja <vikramahuja8803@gmail.com>
2021-05-19 22:32:26 +0530

    Test case fix

--- index/secondary-index/src/test/scala/org/apache/carbondata/spark/testsuite/secondaryindex/TestSIWithPartition.scala
+++ index/secondary-index/src/test/scala/org/apache/carbondata/spark/testsuite/secondaryindex/TestSIWithPartition.scala
@@ -334,17 +334,16 @@ class TestSIWithPartition extends QueryTest with BeforeAndAfterAll {
   }
 
   test("Testing SI on partition table with rename") {
-    sql("alter table rename change empname empAddress string")
     sql("drop index if exists indextable1 on uniqdata1")
     sql("create index indextable1 on table uniqdata1 (DOB, CUST_NAME) AS 'carbondata'")
 
     val withoutIndex =
       sql(
         "select * from uniqdata1 where CUST_NAME='CUST_NAME_00108' and ACTIVE_EMUI_VERSION = " +
-          "'abc'")
+        "'abc'")
         .collect().toSeq
 
-    sql("alter table uniqdata1 change CUST_NAME custs string")
+    sql("alter table uniqdata1 change CUST_NAME test string")
 
     checkAnswer(sql(
       "select * from uniqdata1 where test='CUST_NAME_00108' and ACTIVE_EMUI_VERSION = 'abc'"),
