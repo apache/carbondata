1801b1af9ff5f53cb26976d0f819f64520734b03
Indhumathi27 <indhumathim27@gmail.com>
2022-03-23 11:16:01 +0530

    Don't overwrite SChema file if schema file exists for External table

--- integration/spark/src/test/scala/org/apache/carbondata/spark/testsuite/createTable/TestCreateExternalTable.scala
+++ integration/spark/src/test/scala/org/apache/carbondata/spark/testsuite/createTable/TestCreateExternalTable.scala
@@ -27,6 +27,7 @@ import org.scalatest.BeforeAndAfterAll
 
 import org.apache.carbondata.common.exceptions.sql.MalformedCarbonCommandException
 import org.apache.carbondata.core.constants.CarbonCommonConstants
+import org.apache.carbondata.core.datastore.impl.FileFactory
 import org.apache.carbondata.core.util.CarbonProperties
 
 class TestCreateExternalTable extends QueryTest with BeforeAndAfterAll {
@@ -227,6 +228,44 @@ class TestCreateExternalTable extends QueryTest with BeforeAndAfterAll {
     sql("INSERT INTO source select 100,'spark','test1'")
     checkAnswer(sql("select * from source"), Seq(Row(100, "spark", "test1")))
     sql("drop table if exists source")
+  }
+
+  test("test create external table on transactional table location") {
+    sql("DROP TABLE IF EXISTS table1")
+    sql("DROP TABLE IF EXISTS table2")
+    try {
+      sql("create table table1 (roll string) STORED AS carbondata")
+      sql("insert into table1 values('abc')")
+      val table1 = CarbonEnv.getCarbonTable(Some("default"), "table1")(sqlContext.sparkSession)
+      val lastMdtFileTable1 = FileFactory
+        .getCarbonFile(FileFactory.getUpdatedFilePath(table1.getTablePath + "/Metadata/schema"))
+        .getLastModifiedTime
+      sql(
+        s"""CREATE EXTERNAL TABLE table2 STORED AS carbondata
+           | LOCATION
+           |'${ table1.getTablePath }' """.stripMargin)
+      val table2 = CarbonEnv.getCarbonTable(Some("default"), "table2")(sqlContext.sparkSession)
+      val lastMdtFileTable2 = FileFactory
+        .getCarbonFile(FileFactory.getUpdatedFilePath(table2.getTablePath + "/Metadata/schema"))
+        .getLastModifiedTime
+      assert(lastMdtFileTable1 == lastMdtFileTable2)
+      checkAnswer(sql("select * from table1"), sql("select * from table2"))
+      // verify insert into table1 and check results of table1 and table2
+      sql("insert into table1 values('abcd')")
+      checkAnswer(sql("select * from table1"), sql("select * from table2"))
+      // verify delete from table1 and check results of table1 and table2
+      sql("delete from table1 where roll='abc'")
+      checkAnswer(sql("select * from table1"), sql("select * from table2"))
+      // verify insert into table2 and check results of table1 and table2
+      sql("insert into table2 values('abcde')")
+      checkAnswer(sql("select * from table1"), sql("select * from table2"))
+      // verify delete from table2 and check results of table1 and table2
+      sql("delete from table1 where roll='abcde'")
+      checkAnswer(sql("select * from table1"), sql("select * from table2"))
+    } finally {
+      sql("DROP TABLE IF EXISTS table1")
+      sql("DROP TABLE IF EXISTS table2")
+    }
   }
 
 }
