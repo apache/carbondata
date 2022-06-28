b8511b6279fb86f000c70ad00b109aba2ea2501a
Mahesh Raju Somalaraju <mahesh.somalaraju@huawei.com>
2022-06-28 17:03:39 +0530

    [CARBONDATA-4345] update/delete operations failed when other format segemnt deleted from carbon table
    Why is this PR needed?
    Update/delete operations failed when other format segments deleted from carbon table
    Steps to reproduce:
    1. create carbon table and load the data
    2. create parquet/orc tables and load the data
    3. add parquet/orc format segments in carbon table by alter add segment command
    4. perform update/delete operations in carbon table and they will fail as table
       contains mixed format segments. This is expected behaviour only.
    5. delete the other format segments which is added in step3
    6. try to perform update/delete operation in carbon data. They should not fail
    
    For update/delete operations we are checking if other format segments present
    in table path. If found then carbon data throwing exception by saying mixed
    format segments exists even though the other format segments deleted from table.
    
    What changes were proposed in this PR?
    When we are checking other format segment present in carbon table then it
    should check only for SUCCESS/PARTIAL_SUCCESS segments.
    
    Does this PR introduce any user interface change?
    No
    
    Is any new testcase added?
    Yes
    
    This closes #4285

--- integration/spark/src/main/scala/org/apache/spark/sql/execution/strategy/MixedFormatHandler.scala
+++ integration/spark/src/main/scala/org/apache/spark/sql/execution/strategy/MixedFormatHandler.scala
@@ -399,6 +399,8 @@ object MixedFormatHandler {
    */
   def otherFormatSegmentsExist(metadataPath: String, tableStatusVersion: String): Boolean = {
     val allSegments = SegmentStatusManager.readLoadMetadata(metadataPath, tableStatusVersion)
-    allSegments.exists(a => a.getFileFormat != null && !a.isCarbonFormat)
+    allSegments.exists(a => (a.getSegmentStatus == SegmentStatus.SUCCESS ||
+                             a.getSegmentStatus == SegmentStatus.LOAD_PARTIAL_SUCCESS) &&
+                            a.getFileFormat != null && !a.isCarbonFormat)
   }
 }
